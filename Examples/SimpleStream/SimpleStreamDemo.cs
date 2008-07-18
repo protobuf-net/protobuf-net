a3b733b6fad86f8215c7e5502c05cc92e5268753
marc.gravell <marc.gravell@b72047a4-3652-0410-9617-b3994939e97b>
2008-07-18 12:45:44 +0000

    Trivial Test1 performance loop
    git-svn-id: http://protobuf-net.googlecode.com/svn/trunk@11 b72047a4-3652-0410-9617-b3994939e97b

--- Examples/SimpleStream/SimpleStreamDemo.cs
+++ Examples/SimpleStream/SimpleStreamDemo.cs
@@ -103,6 +103,27 @@ namespace Examples.SimpleStream
             Test1 t1 = new Test1 {A=150};
             TestItem(t1, 0x08, 0x96, 0x01);
 
+
+            const int LOOP = 1000000;
+            Console.WriteLine("\tTesting BinaryFormatter vs ProtoBuf.Serializer...");
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(Stream.Null, t1); // for JIT
+            Stopwatch watch = Stopwatch.StartNew();
+            for (int i = 0; i < LOOP; i++)
+            {
+                bf.Serialize(Stream.Null, t1);
+            }
+            watch.Stop();
+            Console.WriteLine("\tBinaryFormatter x{0}: {1}ms", LOOP, watch.ElapsedMilliseconds);
+            watch = Stopwatch.StartNew();
+            for (int i = 0; i < LOOP; i++)
+            {
+               Serializer.Serialize(t1, Stream.Null);
+            }
+            watch.Stop();
+            Console.WriteLine("\tProtoBuf.Serializer x{0}: {1}ms", LOOP, watch.ElapsedMilliseconds);
+            Console.WriteLine();
+
             Test2 t2 = new Test2 { B = "testing" };
             TestItem(t2, 0x12, 0x07, 0x74, 0x65, 0x73, 0x74, 0x69, 0x6e, 0x67);
 
