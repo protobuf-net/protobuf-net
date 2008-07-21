6541b3c144c30ffc10dd81636d705f625fe29e75
marc.gravell <marc.gravell@b72047a4-3652-0410-9617-b3994939e97b>
2008-07-21 07:45:19 +0000

    explicit Decimal handling, rather than using OA currency
    git-svn-id: http://protobuf-net.googlecode.com/svn/trunk@23 b72047a4-3652-0410-9617-b3994939e97b

--- ProtoBuf/TwosComplementSerializer_Decimal.cs
+++ ProtoBuf/TwosComplementSerializer_Decimal.cs
@@ -2,24 +2,34 @@
 
 namespace ProtoBuf
 {
-    /// Serializes Decimal as an OLE Automation currency
+    /// Serializes Decimal similar to an OLE Automation currency; 4 digits
+    /// after the decimal point
     partial class TwosComplementSerializer : ISerializer<decimal>
     {
+        public static long DecimalToLong(decimal value)
+        {
+            return (long)(value * FACTOR);
+        }
+        const int FACTOR = 10000;
+        public static decimal LongToDecimal(long value)
+        {
+            return new decimal(value) / FACTOR;
+        }
         string ISerializer<decimal>.DefinedType { get { return ProtoFormat.INT64; } }
 
         public decimal Deserialize(decimal value, SerializationContext context)
         {
             long lVal = TwosComplementSerializer.ReadInt64(context);
-            return decimal.FromOACurrency(lVal);
+            return LongToDecimal(lVal);
         }
         public int GetLength(decimal value, SerializationContext context)
         {
-            long lVal = decimal.ToOACurrency(value);
+            long lVal = DecimalToLong(value);
             return TwosComplementSerializer.GetLength(lVal);
         }
         public int Serialize(decimal value, SerializationContext context)
         {
-            long lVal = decimal.ToOACurrency(value);
+            long lVal = DecimalToLong(value);
             return TwosComplementSerializer.WriteToStream(lVal, context);
         }
     }
--- ProtoBuf/ZigZagSerializer_Decimal.cs
+++ ProtoBuf/ZigZagSerializer_Decimal.cs
@@ -8,16 +8,16 @@ namespace ProtoBuf
         public decimal Deserialize(decimal value, SerializationContext context)
         {
             long lVal = ZigZagSerializer.ReadInt64(context);
-            return decimal.FromOACurrency(lVal);
+            return TwosComplementSerializer.LongToDecimal(lVal);
         }
         public int GetLength(decimal value, SerializationContext context)
         {
-            long lVal = decimal.ToOACurrency(value);
+            long lVal = TwosComplementSerializer.DecimalToLong(value);
             return ZigZagSerializer.GetLength(lVal);
         }
         public int Serialize(decimal value, SerializationContext context)
         {
-            long lVal = decimal.ToOACurrency(value);
+            long lVal = TwosComplementSerializer.DecimalToLong(value);
             return ZigZagSerializer.WriteToStream(lVal, context);
         }
     }
