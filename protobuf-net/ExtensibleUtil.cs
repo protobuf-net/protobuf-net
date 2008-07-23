2e38baf6f64023b643f84ef55b0606008d329c9f
marc.gravell <marc.gravell@b72047a4-3652-0410-9617-b3994939e97b>
2008-07-23 08:44:33 +0000

    Push/pop when appending extended values [recursion detection kicks in at the first repeat, rather than the second]; documentation for ExtensibleUtil
    git-svn-id: http://protobuf-net.googlecode.com/svn/trunk@71 b72047a4-3652-0410-9617-b3994939e97b

--- protobuf-net/ExtensibleUtil.cs
+++ protobuf-net/ExtensibleUtil.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.IO;
+using System.Reflection;
 using System.Runtime.Serialization;
 
 namespace ProtoBuf
@@ -14,15 +14,30 @@ namespace ProtoBuf
     /// </summary>
     internal static class ExtensibleUtil
     {
-        public static IEnumerable<TValue> GetExtendedValues<TValue>(IExtensible instance, int tag, DataFormat format, bool singleton)
+        /// <summary>
+        /// All this does is call GetExtendedValuesTyped with the correct type for "instance";
+        /// this ensures that we don't get issues with subclasses declaring conflicting types -
+        /// the caller must respect the fields defined for the type they pass in.
+        /// </summary>
+        internal static IEnumerable<TValue> GetExtendedValues<TValue>(IExtensible instance, int tag, DataFormat format, bool singleton)
         {
             if (instance == null) throw new ArgumentNullException("instance");
             return (IEnumerable<TValue>)typeof(ExtensibleUtil)
-                .GetMethod("GetExtendedValuesTyped")
+                .GetMethod("GetExtendedValuesTyped", BindingFlags.Public | BindingFlags.Static)
                 .MakeGenericMethod(instance.GetType(), typeof(TValue))
                 .Invoke(null, new object[] { instance, tag, format, singleton });
         }
-        public static IEnumerable<TValue> GetExtendedValuesTyped<TSource, TValue>(TSource instance, int tag, DataFormat format, bool singleton)
+
+        /// <summary>
+        /// Reads the given value(s) from the instance's stream; the serializer
+        /// is inferred from TValue and format. For singletons, each occurrence
+        /// is merged [only really matters for sub-objects], and the composed
+        /// value if yielded once; otherwise ("repeated") each occurrence
+        /// is yielded separately.
+        /// </summary>
+        /// <remarks>Needs to be public to be callable thru reflection in Silverlight</remarks>
+        public static IEnumerable<TValue> GetExtendedValuesTyped<TSource, TValue>(
+            TSource instance, int tag, DataFormat format, bool singleton)
             where TSource : class, IExtensible, new()
         {
             Serializer<TSource>.CheckTagNotInUse(tag);
@@ -79,32 +94,47 @@ namespace ProtoBuf
             }
         }
 
-        public static void AppendExtendValue<TValue>(IExtensible instance, int tag, DataFormat format, object value)
+        /// <summary>
+        /// All this does is call AppendExtendValueTyped with the correct type for "instance";
+        /// this ensures that we don't get issues with subclasses declaring conflicting types -
+        /// the caller must respect the fields defined for the type they pass in.
+        /// </summary>
+        internal static void AppendExtendValue<TValue>(IExtensible instance, int tag, DataFormat format, object value)
         {
             if (instance == null) throw new ArgumentNullException("instance");
             typeof(ExtensibleUtil)
-                .GetMethod("AppendExtendValueTyped")
+                .GetMethod("AppendExtendValueTyped", BindingFlags.Public | BindingFlags.Static)
                 .MakeGenericMethod(instance.GetType(), typeof(TValue))
                 .Invoke(null, new object[] { instance, tag, format, value});
         }
-        public static void AppendExtendValueTyped<TSource, TValue>(TSource instance, int tag, DataFormat format, TValue value)
+        /// <summary>
+        /// Stores the given value into the instance's stream; the serializer
+        /// is inferred from TValue and format.
+        /// </summary>
+        /// <remarks>Needs to be public to be callable thru reflection in Silverlight</remarks>
+        public static void AppendExtendValueTyped<TSource, TValue>(
+            TSource instance, int tag, DataFormat format, TValue value)
             where TSource : class, IExtensible, new()
         {
             Serializer<TSource>.CheckTagNotInUse(tag);
-
             ISerializer<TValue> serializer = SerializerCache<TValue>.GetSerializer(format);
 
+            // check the length of the new value, using the null stream as a stop-gap
             SerializationContext nullCtx = new SerializationContext(Stream.Null);
             int len = serializer.GetLength(value, nullCtx);
+
             if (len > 0)
             {
+                // something worth writing; grab a stream from the instance and append
                 Stream stream = instance.BeginAppend();
                 try
                 {
                     SerializationContext ctx = new SerializationContext(stream);
+                    ctx.Push(instance); // for recursion detection
                     Serializer.WriteFieldToken(tag, serializer.WireType, ctx);
                     serializer.Serialize(value, ctx);
                     instance.EndAppend(stream, true);
+                    ctx.Pop(instance);
                 }
                 catch
                 {
