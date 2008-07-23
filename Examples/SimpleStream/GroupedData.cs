093aeef171a27ad07f705bc8765a355c2818ed19
marc.gravell <marc.gravell@b72047a4-3652-0410-9617-b3994939e97b>
2008-07-23 11:18:09 +0000

    grouped unit tests; missed in last commit
    git-svn-id: http://protobuf-net.googlecode.com/svn/trunk@80 b72047a4-3652-0410-9617-b3994939e97b

---
+++ Examples/SimpleStream/GroupedData.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using NUnit.Framework;
+using ProtoBuf;
+
+namespace Examples.SimpleStream
+{
+    [TestFixture]
+    public class GroupedData
+    {
+        [Test]
+        public void TestGroup()
+        {
+            Test3 t3 = Program.Build<Test3>(0x1B, 0x08, 0x96, 0x01, 0x1C);// [start group 3] [test1] [end group 3]
+            Assert.AreEqual(150, t3.C.A);
+
+        }
+        [Test, ExpectedException(typeof(SerializationException))]
+        public void TestUnterminatedGroup()
+        {
+            Test3 t3 = Program.Build<Test3>(0x1B, 0x08, 0x96, 0x01 );// [start group 3] [test1]
+        }
+        [Test, ExpectedException(typeof(SerializationException))]
+        public void TestWrongGroupClosed()
+        {
+            Test3 t3 = Program.Build<Test3>( 0x1B, 0x08, 0x96, 0x01, 0x24 );// [start group 3] [test1] [end group 4]
+        }
+
+        [ProtoContract]
+        class Test3List
+        {
+            [ProtoMember(3)]
+            public List<Test1> C { get; set; }
+        }
+
+        [ProtoContract]
+        class Test1List
+        {
+            [ProtoMember(1)]
+            public List<int> A { get; set; }
+        }
+        [Test]
+        public void TestEntityList()
+        {
+            Test3List t3 = Program.Build<Test3List>(
+                0x1B, 0x08, 0x96, 0x01, 0x1C, // start 3: A=150; end 3
+                0x1B, 0x08, 0x82, 0x01, 0x1C);// start 3: A=130; end 3
+            Assert.AreEqual(2, t3.C.Count);
+            Assert.AreEqual(150, t3.C[0].A);
+            Assert.AreEqual(130, t3.C[1].A);
+        }
+
+        [Test, ExpectedException(typeof(SerializationException))]
+        public void TestPrimativeList()
+        {
+            Test1List t1 = Program.Build<Test1List>(0x0B, 0x96, 0x01, 0x0C); // [start:1] [150] [end:1]
+        }
+    }
+}
