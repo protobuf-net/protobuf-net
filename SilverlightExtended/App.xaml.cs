1841812a785f05bce168727484d97c457d1cb561
xmlhacker <xmlhacker@b72047a4-3652-0410-9617-b3994939e97b>
2008-07-28 00:57:23 +0000

    missed as part of the last checkin
    git-svn-id: http://protobuf-net.googlecode.com/svn/trunk@100 b72047a4-3652-0410-9617-b3994939e97b

--- SilverlightExtended/App.xaml.cs
+++ SilverlightExtended/App.xaml.cs
@@ -1,14 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 
 namespace SilverlightExtended
 {
