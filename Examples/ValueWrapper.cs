89b429c3aa4eb36831e0865588ea37f9d7650b99
marc.gravell <marc.gravell@b72047a4-3652-0410-9617-b3994939e97b>
2009-02-19 05:57:52 +0000

    Nullable<T> tests of value-wrapper
    git-svn-id: http://protobuf-net.googlecode.com/svn/trunk@238 b72047a4-3652-0410-9617-b3994939e97b

--- Examples/ValueWrapper.cs
+++ Examples/ValueWrapper.cs
@@ -50,6 +50,41 @@ namespace Examples
     }
 
     [ProtoContract]
+    public class FieldDataViaNullable
+    {
+        public FieldDataViaNullable() { }
+        public FieldDataViaNullable(object value)
+        {
+            Value = value;
+        }
+        public object Value { get; set; }
+
+        private T? Get<T>() where T : struct
+        {
+            return (Value != null && Value is T) ? (T?)Value : (T?)null;
+        }
+        [ProtoMember(1)]
+        private int? ValueInt32
+        {
+            get { return Get<int>(); }
+            set { Value = value; }
+        }
+        [ProtoMember(2)]
+        private float? ValueSingle
+        {
+            get { return Get<float>(); }
+            set { Value = value; }
+        }
+        [ProtoMember(3)]
+        private double? ValueDouble
+        {
+            get { return Get<double>(); ; }
+            set { Value = value; }
+        }
+        // etc for expected types
+    }
+
+    [ProtoContract]
     class Int32Simple
     {
         [ProtoMember(1)]
@@ -106,6 +141,49 @@ namespace Examples
             Assert.IsTrue(Program.CheckBytes(new FieldData { Value = 123.45 },
                 GetBytes(new DoubleSimple { Value = 123.45 })), "Double");
             Assert.AreEqual(123.45, Serializer.DeepClone(new FieldData(123.45)).Value);
+        }
+
+    }
+
+    [TestFixture]
+    public class ValueWrapperTestsViaNullable
+    {
+        static byte[] GetBytes<T>(T item)
+        {
+            MemoryStream ms = new MemoryStream();
+            Serializer.Serialize(ms, item);
+            return ms.ToArray();
+        }
+        [Test]
+        public void TestRaw()
+        {
+            Assert.IsTrue(Program.CheckBytes(new FieldDataViaNullable()), "Empty");
+            Assert.AreEqual(null, Serializer.DeepClone(new FieldDataViaNullable()).Value);
+
+        }
+
+        [Test]
+        public void TestInt32()
+        {
+
+            Assert.IsTrue(Program.CheckBytes(new FieldDataViaNullable {Value = 123},
+                                             GetBytes(new Int32Simple {Value = 123})), "Int32");
+            Assert.AreEqual(123, Serializer.DeepClone(new FieldDataViaNullable(123)).Value);
+        }
+        [Test]
+        public void TestSingle()
+        {
+            Assert.IsTrue(Program.CheckBytes(new FieldDataViaNullable {Value = 123.45F},
+                                             GetBytes(new SingleSimple {Value = 123.45F})), "Single");
+            Assert.AreEqual(123.45F, Serializer.DeepClone(new FieldDataViaNullable(123.45F)).Value);
+
+        }
+        [Test]
+        public void TestDouble()
+        {
+            Assert.IsTrue(Program.CheckBytes(new FieldDataViaNullable { Value = 123.45 },
+                GetBytes(new DoubleSimple { Value = 123.45 })), "Double");
+            Assert.AreEqual(123.45, Serializer.DeepClone(new FieldDataViaNullable(123.45)).Value);
         }
 
     }
