8384a27e02d7fd9b2a6156c9583a34c3453baffc
marc.gravell <marc.gravell@b72047a4-3652-0410-9617-b3994939e97b>
2009-02-24 21:18:20 +0000

    Issue45.cs (Assembly.LoadFrom)
    git-svn-id: http://protobuf-net.googlecode.com/svn/trunk@239 b72047a4-3652-0410-9617-b3994939e97b

---
+++ Examples/Issues/Issue45.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using NUnit.Framework;
+using System;
+using ProtoBuf;
+
+namespace Examples.Issues
+{
+    [TestFixture]
+    public class LateLoadedTests
+    {
+        [Test]
+        public void TestLateLoad()
+        {
+            Assembly assembly = Assembly.LoadFrom("LateLoaded.dll");
+            Type type = assembly.GetType("LateLoaded.Foo");
+            Assert.IsNotNull(type, "Resolve type");
+
+            object obj = Activator.CreateInstance(type);
+            const string EXPECTED = "Some value";
+            type.GetProperty("BaseProp").SetValue(obj, EXPECTED, null);
+
+            MethodInfo method = typeof(Serializer).GetMethod("DeepClone").MakeGenericMethod(type);
+
+            object clone = method.Invoke(null, new object[] { obj });
+            Assert.IsNotNull(clone, "Create clone");
+            Assert.AreNotSame(obj, clone, "Clone different instance");
+            Assert.IsInstanceOfType(type, clone, "Clone correct type");
+            object value = type.GetProperty("BaseProp").GetValue(clone, null);
+            Assert.AreEqual(EXPECTED, value, "Clone value");
+        }
+    }
+}
---
+++ LateLoaded/FooBar.cs
@@ -0,0 +1,19 @@
+﻿
+using ProtoBuf;
+namespace LateLoaded
+{
+    [ProtoContract]
+    [ProtoInclude(2, typeof(Bar))]
+    public class Foo
+    {
+        [ProtoMember(1)]
+        public string BaseProp { get; set; }
+    }
+
+    [ProtoContract]
+    public class Bar : Foo
+    {
+        [ProtoMember(1)]
+        public string ChildProp { get; set; }
+    }
+}
