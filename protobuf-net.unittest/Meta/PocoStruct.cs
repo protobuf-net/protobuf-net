082f7c5acf2aad849f7d4957efa4a2b3c6cc58dd
marc.gravell <marc.gravell@b72047a4-3652-0410-9617-b3994939e97b>
2010-05-29 23:25:45 +0000

    Prove that previous commit also fixes PocoStruct test (was the same stack-corruption issue)
    git-svn-id: http://protobuf-net.googlecode.com/svn/trunk@337 b72047a4-3652-0410-9617-b3994939e97b

--- protobuf-net.unittest/Meta/PocoStruct.cs
+++ protobuf-net.unittest/Meta/PocoStruct.cs
@@ -7,9 +7,8 @@ using ProtoBuf.Meta;
 namespace ProtoBuf.unittest.Meta
 {
     [TestFixture]
-    public class PocoStruct
+    public class PocoClass
     {
-        //TODO do this as a struct - some glitch
         public class Company
         {
             private readonly List<Employee> employees = new List<Employee>();
@@ -40,6 +39,48 @@ namespace ProtoBuf.unittest.Meta
                 ms.Position = 0;
                 Console.WriteLine("Bytes: " + ms.Length);
                 clone = (Company) model.Deserialize(ms, null, typeof(Company));
+            }
+            Assert.AreEqual(3, clone.Employees.Count);
+            Assert.AreEqual("Boss", clone.Employees[0].Designation);
+            Assert.AreEqual("Alex", clone.Employees[2].EmployeeName);
+        }
+    }
+
+    [TestFixture]
+    public class PocoStruct
+    {
+        public struct Company
+        {
+            private List<Employee> employees;
+            public List<Employee> Employees { get { return employees ?? (employees = new List<Employee>()); } }
+        }
+
+        public struct Employee
+        {
+            public string EmployeeName { get; set; }
+            public string Designation { get; set; }
+        }
+        [Test]
+        public void CanSerializeCompany()
+        {
+            var model = TypeModel.Create();
+            model.Add(typeof(Company), false).Add("Employees");
+            model.Add(typeof(Employee), false).Add("EmployeeName", "Designation");
+            model.CompileInPlace();
+
+            Company comp = new Company
+            {
+                Employees = {
+                    new Employee { Designation = "Boss", EmployeeName = "Fred"},
+                    new Employee { Designation = "Grunt", EmployeeName = "Jo"},
+                    new Employee { Designation = "Scapegoat", EmployeeName = "Alex"}}
+            }, clone;
+            using (var ms = new MemoryStream())
+            {
+                model.Serialize(ms, comp);
+                ms.Position = 0;
+                Console.WriteLine("Bytes: " + ms.Length);
+                clone = (Company)model.Deserialize(ms, null, typeof(Company));
             }
             Assert.AreEqual(3, clone.Employees.Count);
             Assert.AreEqual("Boss", clone.Employees[0].Designation);
