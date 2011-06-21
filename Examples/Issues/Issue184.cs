3b6740c1d774f69f7a63741e25016d4f9bb1b6e2
marc.gravell <marc.gravell@b72047a4-3652-0410-9617-b3994939e97b>
2011-06-21 06:14:07 +0000

    TEST: Issue191 (already fixed separately)
    git-svn-id: http://protobuf-net.googlecode.com/svn/trunk@420 b72047a4-3652-0410-9617-b3994939e97b

--- Examples/Issues/Issue184.cs
+++ Examples/Issues/Issue184.cs
@@ -13,7 +13,7 @@ namespace Examples.Issues
     [TestFixture]
     public class Issue184
     {
-        [Test, ExpectedException(typeof(ArgumentException), ExpectedMessage="IEnumerable[<T>] data cannot be used as a meta-type unless an Add method can be resolved")]
+        [Test, ExpectedException(typeof(ArgumentException), ExpectedMessage = "IEnumerable[<T>] data cannot be used as a meta-type unless an Add method can be resolved")]
         public void CantCreateUnusableEnumerableMetaType()
         {
             var model = TypeModel.Create();
@@ -30,6 +30,62 @@ namespace Examples.Issues
         {
             var model = TypeModel.Create();
             model.Add(typeof(IList<int>), false).AddSubType(5, typeof(List<int>));
-        }        
+        }
+
+
+        public interface IMobileObject { }
+        public class MobileList<T> : List<T>, IMobileObject
+        {
+            public override bool Equals(object obj) { return this.SequenceEqual((IEnumerable<T>)obj); }
+        }
+        [ProtoContract]
+        public class A : IMobileObject
+        {
+            [ProtoMember(1)]
+            public int X { get; set; }
+            public override bool Equals(object obj) { return ((A)obj).X == X; }
+            public override string ToString()
+            {
+                return X.ToString();
+            }
+        }
+        [ProtoContract]
+        public class B
+        {
+            [ProtoMember(1)]
+            public List<IMobileObject> Objects { get; set; }
+        }
+        //[Test]
+        //public void Execute()
+        //{
+        //    var m = TypeModel.Create();
+        //    m.AutoCompile = false;                 
+        //    m.Add(typeof(IMobileObject), false).AddSubType(1, typeof(A)).AddSubType(2, typeof(MobileList<int>));
+
+        //    TestListAsSubclass(m, "Runtime");
+
+        //    m.CompileInPlace();
+        //    TestListAsSubclass(m, "CompileInPlace");
+
+        //    TestListAsSubclass(m.Compile(), "Compile");
+
+        //    m.Compile("Issue184", "Issue184.dll");
+        //    PEVerify.AssertValid("Issue184.dll");
+        //}
+
+        //private static void TestListAsSubclass(TypeModel m, string caption)
+        //{
+        //    var b = new B { Objects = new List<IMobileObject> { new A { X = 3 }, new A { X = 17 }, new MobileList<int> { 3, 7 } } };
+        //    using (var ms = new MemoryStream())
+        //    {
+        //        m.Serialize(ms, b);
+        //        ms.Position = 0;
+        //        var b2 = (B)m.Deserialize(ms, null, typeof(B));
+        //        Assert.AreEqual(3, b2.Objects.Count, caption);
+        //        Assert.AreEqual(b.Objects[0], b2.Objects[0], caption);
+        //        Assert.AreEqual(b.Objects[1], b2.Objects[1], caption);
+        //        Assert.AreEqual(b.Objects[2], b2.Objects[2], caption);
+        //    }
+        //}
     }
 }
---
+++ Examples/Issues/Issue191.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using ProtoBuf.Meta;
+using ProtoBuf;
+
+namespace Examples.Issues
+{
+    [TestFixture]
+    public class Issue191
+    {
+        [ProtoContract]
+        public class Test
+        {
+            // Deserializes as null
+            [ProtoMember(1)]
+            public Dictionary<long, string> dict = new Dictionary<long, string>();
+
+
+            // Deserializes correctly
+            [ProtoMember(2)]
+            public Dictionary<long, string> dict2 { get; set; }
+            public Test()
+            {
+                this.dict2 = new Dictionary<long, string>();
+            }
+        }
+        [Test]
+        public void Execute()
+        {
+            var model = TypeModel.Create();
+            model.AutoCompile = false;
+            model.Add(typeof(Test), true);
+
+            RoundtripEmptyDictionaryShouldNotNullThem(model, "Runtime");
+            model.CompileInPlace();
+            RoundtripEmptyDictionaryShouldNotNullThem(model, "CompileInPlace");
+
+            RoundtripEmptyDictionaryShouldNotNullThem(model.Compile(), "Compile");
+        }
+        public void RoundtripEmptyDictionaryShouldNotNullThem(TypeModel model, string scenario)
+        {
+            var orig = new Test();
+            Assert.IsNotNull(orig.dict, scenario);
+            Assert.AreEqual(0, orig.dict.Count, scenario);
+            Assert.IsNotNull(orig.dict2, scenario);
+            Assert.AreEqual(0, orig.dict2.Count, scenario);
+
+            var clone = (Test)model.DeepClone(orig);
+            Assert.IsNotNull(clone.dict, scenario);
+            Assert.AreEqual(0, clone.dict.Count, scenario);
+            Assert.IsNotNull(clone.dict2, scenario);
+            Assert.AreEqual(0, clone.dict2.Count, scenario);
+
+        }
+    }
+}
