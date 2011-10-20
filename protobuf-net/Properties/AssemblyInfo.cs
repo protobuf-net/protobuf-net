ac09fb790ecb6061ecddc7b5d2df632cffdbfca0
marc.gravell <marc.gravell@b72047a4-3652-0410-9617-b3994939e97b>
2011-10-20 20:02:27 +0000

    Nuget packaging
    git-svn-id: http://protobuf-net.googlecode.com/svn/trunk@453 b72047a4-3652-0410-9617-b3994939e97b

--- protobuf-net/Properties/AssemblyInfo.cs
+++ protobuf-net/Properties/AssemblyInfo.cs
@@ -35,9 +35,9 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.450")]
+[assembly: AssemblyVersion("2.0.0.452")]
 #if !CF
-[assembly: AssemblyFileVersion("2.0.0.450")]
+[assembly: AssemblyFileVersion("2.0.0.452")]
 #endif
 #if !FX11
 [assembly: InternalsVisibleTo("protobuf-net.unittest, PublicKey="
