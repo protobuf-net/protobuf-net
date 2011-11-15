f78abc85b273a7927caede8437d83541e0ec2a6a
marc.gravell <marc.gravell@b72047a4-3652-0410-9617-b3994939e97b>
2011-11-15 20:42:37 +0000

    Added tests; SO8093623.cs (known to need more work), and ListsWithInheritance.cs (passes; no-repro)
    git-svn-id: http://protobuf-net.googlecode.com/svn/trunk@471 b72047a4-3652-0410-9617-b3994939e97b

---
+++ Examples/Issues/SO8093623.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using ProtoBuf;
+using System.IO;
+using ProtoBuf.Meta;
+
+namespace Examples.Issues
+{
+    [TestFixture]
+    public class SO8093623
+    {
+        [ProtoContract]
+        public class A_generated
+        {
+            [ProtoMember(1)]
+            public int Age;
+
+            [ProtoMember(10)]
+            public B_generated b;
+        }
+
+        [ProtoContract]
+        public class B_generated
+        {
+            [ProtoMember(2)]
+            public int Balls;
+        }
+        [ProtoContract, ProtoInclude(10, typeof(B))]
+        public class A
+        {
+            [ProtoMember(1)]
+            public int Age;
+        }
+
+        public class B : A
+        {
+            [ProtoMember(2)]
+            public int Balls;
+        }
+
+        [Test]
+        public void TestExpectedResultFromGeneratedTypes()
+        {
+            var model = RuntimeTypeModel.Create();
+            model.AutoCompile = false;
+            model.Add(typeof(A_generated), true);
+            model.Add(typeof(B_generated), true);
+
+            TestGeneratedModel(model, "Runtime");
+            model.CompileInPlace();
+            TestGeneratedModel(model, "CompileInPlace");
+            TestGeneratedModel(model.Compile(), "Compile");
+        }
+        private static void TestGeneratedModel(TypeModel model, string message)
+        {
+            var a = new A_generated() { Age = 10, b = new B_generated { Balls = 23 } };
+            using (var ms = new MemoryStream())
+            {
+                model.Serialize(ms, a);
+                Assert.IsTrue(ms.ToArray().SequenceEqual(new byte[] { 08, 10, 82, 2, 16, 23 }), message);
+                ms.Position = 0;
+                var clone = (A_generated)model.Deserialize(ms, null, typeof(A_generated));
+                Assert.AreEqual(10, clone.Age, message);
+                Assert.AreEqual(23, clone.b.Balls, message);
+            }
+        }
+        [Test]
+        public void TestSubclassDeserializes()
+        {
+            var model = RuntimeTypeModel.Create();
+            model.AutoCompile = false;
+            model.Add(typeof (A), true);
+            model.Add(typeof (B), true);
+
+            TestInheritanceModel(model, "Runtime");
+            model.CompileInPlace();
+            TestInheritanceModel(model, "CompileInPlace");
+            TestInheritanceModel(model.Compile(), "Compile");
+        }
+
+        private static void TestInheritanceModel(TypeModel model, string message)
+        {
+            using (var ms = new MemoryStream(new byte[] {08, 10, 82, 2, 16, 23}))
+            {
+                var clone = (A)model.Deserialize(ms, null, typeof(A));
+                Assert.AreEqual(10, clone.Age, message);
+                B b = (B) clone;
+                Assert.AreEqual(23, b.Balls, message);
+            }
+        }
+    }
+}
---
+++ Examples/ListsWithInheritance.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using ProtoBuf;
+
+namespace Examples
+{
+    [TestFixture]
+    public class ListsWithInheritance
+    {
+        [Test]
+        public void TestBasicRoundtripViaDataClass()
+        {
+            Data data = new Data();
+            data.Parties.Add(new Debtor());
+            data.Parties.Add(new Party());
+            data.Parties.Add(new Creditor());
+            var clone = Serializer.DeepClone(data);
+
+            Assert.AreEqual(3, clone.Parties.Count);
+            Assert.AreEqual(typeof(Debtor), clone.Parties[0].GetType());
+            Assert.AreEqual(typeof(Party), clone.Parties[1].GetType());
+            Assert.AreEqual(typeof(Creditor), clone.Parties[2].GetType());
+        }
+
+        [Test]
+        public void TestBasicRoundtripOfNakedList()
+        {
+            var list = new List<Party>();
+            list.Add(new Debtor());
+            list.Add(new Party());
+            list.Add(new Creditor());
+            var clone = Serializer.DeepClone(list);
+
+            Assert.AreEqual(3, clone.Count);
+            Assert.AreEqual(typeof(Debtor), clone[0].GetType());
+            Assert.AreEqual(typeof(Party), clone[1].GetType());
+            Assert.AreEqual(typeof(Creditor), clone[2].GetType());
+        }
+
+        [ProtoContract]
+        public class Data
+        {
+            [ProtoMember(1)]
+            public List<Party> Parties { get { return parties; } }
+
+            private readonly List<Party> parties = new List<Party>();
+        }
+
+        [ProtoContract]
+        [ProtoInclude(1, typeof(Party))]
+        public class BaseClass
+        {
+        }
+        [ProtoContract]
+        [ProtoInclude(1, typeof(Creditor))]
+        [ProtoInclude(2, typeof(Debtor))]
+        public class Party : BaseClass
+        {
+        }
+        [ProtoContract]
+        public class Creditor : Party
+        {
+        }
+        [ProtoContract]
+        public class Debtor : Party
+        {
+        }
+    }
+}
