5d2cf396e33a18150c9ec2593cfaa241e728daf7
marc.gravell <marc.gravell@b72047a4-3652-0410-9617-b3994939e97b>
2012-08-09 20:41:01 +0000

    569 deploy (NuGet, pre-release); includes test for issue 312
    git-svn-id: http://protobuf-net.googlecode.com/svn/trunk@569 b72047a4-3652-0410-9617-b3994939e97b

---
+++ Examples/Issues/Issue312.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using ProtoBuf;
+using ProtoBuf.Meta;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Examples.Issues
+{
+    [TestFixture]
+    public class Issue312
+    {
+        [ProtoContract]
+        public class RootClass
+        {
+            [ProtoMember(1)]
+            public int Prop1 { get; set; }
+            [ProtoMember(2)]
+            public string Prop2 { get; set; }
+            [ProtoMember(3)]
+            public IEnumerableClass Prop3 { get; set; }
+        }
+
+        [ProtoContract(IgnoreListHandling = true)]
+        public class IEnumerableClass : IEnumerable<char>
+        {
+            [ProtoMember(1)]
+            public int Prop1 { get; set; }
+            [ProtoMember(2)]
+            public string Prop2 { get; set; }
+
+            public IEnumerator<char> GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            // Comment out this indexed property to prevent the crash
+            public char this[int i] { get { return Prop2[i]; } }
+
+            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [Test]
+        public void Execute()
+        {
+            var rt = TypeModel.Create();
+            rt.Add(typeof(IEnumerableClass), true);
+            rt.Add(typeof(RootClass), true);
+            rt.Compile();
+            var c1 = new IEnumerableClass() { Prop1 = 1, Prop2 = "a" };
+            var i1 = new RootClass() { Prop1 = 1, Prop2 = "blabla", Prop3 = c1 };
+            var cloned = rt.DeepClone(i1) as RootClass;
+            Assert.AreEqual(1, cloned.Prop3.Prop1);
+        }
+    }
+}
--- protobuf-net/Properties/AssemblyInfo.cs
+++ protobuf-net/Properties/AssemblyInfo.cs
@@ -35,9 +35,9 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.565")]
+[assembly: AssemblyVersion("2.0.0.569")]
 #if !CF
-[assembly: AssemblyFileVersion("2.0.0.564")]
+[assembly: AssemblyFileVersion("2.0.0.569")]
 #endif
 #if !FX11
 [assembly: InternalsVisibleTo("protobuf-net.unittest, PublicKey="
