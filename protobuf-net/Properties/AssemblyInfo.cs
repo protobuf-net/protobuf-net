b7d111ac73ed6903be57a579c5c2d92102c0a4cc
marc.gravell@gmail.com <marc.gravell@gmail.com@b72047a4-3652-0410-9617-b3994939e97b>
2012-11-06 11:50:23 +0000

    Nuget package for phone 8 and netcore
    git-svn-id: http://protobuf-net.googlecode.com/svn/trunk@601 b72047a4-3652-0410-9617-b3994939e97b

--- protobuf-net/Properties/AssemblyInfo.cs
+++ protobuf-net/Properties/AssemblyInfo.cs
@@ -35,9 +35,9 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.594")]
+[assembly: AssemblyVersion("2.0.0.601")]
 #if !CF
-[assembly: AssemblyFileVersion("2.0.0.594")]
+[assembly: AssemblyFileVersion("2.0.0.601")]
 #endif
 #if !FX11
 [assembly: InternalsVisibleTo("protobuf-net.unittest, PublicKey="
