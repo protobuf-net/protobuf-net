51334ee20bdf39cc7aa9e07b33e9fc98931b7fd0
marc.gravell <marc.gravell@b72047a4-3652-0410-9617-b3994939e97b>
2013-05-28 11:40:38 +0000

    deploy r638
    git-svn-id: http://protobuf-net.googlecode.com/svn/trunk@638 b72047a4-3652-0410-9617-b3994939e97b

---
+++ Examples/Issues/SO16756104.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples.Issues
+{
+    [TestFixture]
+    public class SO16756104
+    {
+        [Test, ExpectedException(typeof(NullReferenceException))]
+        public void TestNullableDoubleList()
+        {
+            var list = new List<double?> { 1, null, 2 };
+            Serializer.DeepClone(list);
+        }
+
+        [Test, ExpectedException(typeof(NullReferenceException))]
+        public void TestNullableInt32List()
+        {
+            var list = new List<int?> { 1, null, 2 };
+            Serializer.DeepClone(list);
+        }
+
+        [Test, ExpectedException(typeof(NullReferenceException))]
+        public void TestNullableStringList()
+        {
+            var list = new List<string> { "abc", null, "def" };
+            Serializer.DeepClone(list);
+        }
+    }
+}
