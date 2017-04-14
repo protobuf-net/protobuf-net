d758fc68f80f2a5f95a50cf2501af39a8301ab9e
Marc Gravell <marc.gravell@gmail.com>
2017-04-14 13:09:16 +0100

    Writer api started

--- src/AsyncSpike/SimpleUsage.cs
+++ src/AsyncSpike/SimpleUsage.cs
@@ -7,18 +7,25 @@ using System.Diagnostics;
 using System.IO;
 using System.IO.Pipelines;
 using System.IO.Pipelines.Text.Primitives;
+using System.Text;
+using System.Text.Utf8;
 using System.Threading.Tasks;
+using Xunit;
 
-
-
-public class SimpleUsage
+public class SimpleUsage : IDisposable
 {
+    private PipeFactory _factory = new PipeFactory();
+    void IDisposable.Dispose() => _factory.Dispose();
+
     static void Main()
     {
         try
         {
             Console.WriteLine("Running...");
-            new SimpleUsage().RunGoogleTests().Wait();
+            using (var rig = new SimpleUsage())
+            {
+                rig.RunGoogleTests().Wait();
+            }
         }
         catch (Exception ex)
         {
@@ -26,26 +33,34 @@ public class SimpleUsage
         }
     }
     // see example in: https://developers.google.com/protocol-buffers/docs/encoding
-
     public async Task RunGoogleTests()
     {
-        await Console.Out.WriteLineAsync(nameof(RunTest1));
-        await RunTest1();
+        await Console.Out.WriteLineAsync(nameof(ReadTest1));
+        await ReadTest1();
 
         await Console.Out.WriteLineAsync();
-        await Console.Out.WriteLineAsync(nameof(RunTest2));
-        await RunTest2();
+        await Console.Out.WriteLineAsync(nameof(ReadTest2));
+        await ReadTest2();
 
         await Console.Out.WriteLineAsync();
-        await Console.Out.WriteLineAsync(nameof(RunTest3));
-        await RunTest3();
+        await Console.Out.WriteLineAsync(nameof(ReadTest3));
+        await ReadTest3();
+
+        await Console.Out.WriteLineAsync(nameof(WriteTest1));
+        await WriteTest1();
+
+        await Console.Out.WriteLineAsync();
+        await Console.Out.WriteLineAsync(nameof(WriteTest2));
+        await WriteTest2();
+
     }
 
     [Xunit.Fact]
-    public async Task RunTest1()
-    {
-        await RunTest("08 96 01", reader => Deserialize<Test1>(reader, DeserializeTest1Async));
-    }
+    public Task ReadTest1() => ReadTest<Test1>("08 96 01", DeserializeTest1Async, "A: 150");
+
+    [Xunit.Fact]
+    public Task WriteTest1() => WriteTest(new Test1 { A = 150 }, "08 96 01", SerializeTest1Async);
+
     [Conditional("VERBOSE")]
     static void Trace(string message)
     {
@@ -76,6 +91,27 @@ public class SimpleUsage
         return value ?? Create(ref value);
     }
 
+    async ValueTask<long> SerializeTest1Async(AsyncProtoWriter writer, Test1 value)
+    {
+        long bytes = 0;
+        if (value != null)
+        {
+            Trace("Writing fields...");
+            bytes += await writer.WriteVarintInt32Async(1, value.A);
+        }
+        return bytes;
+    }
+    async ValueTask<long> SerializeTest2Async(AsyncProtoWriter writer, Test2 value)
+    {
+        long bytes = 0;
+        if (value != null)
+        {
+            Trace("Writing fields...");
+            bytes += await writer.WriteStringAsync(2, value.B);
+        }
+        return bytes;
+    }
+
     class Test1
     {
         public int A;
@@ -83,50 +119,52 @@ public class SimpleUsage
     }
 
     [Xunit.Fact]
-    public async Task RunTest2()
-    {
-        await RunTest("12 07 74 65 73 74 69 6e 67", reader => Deserialize<Test2>(reader, DeserializeTest2Async));
-    }
-
-    static async ValueTask<bool> Deserialize<T>(IPipeReader reader, Func<AsyncProtoReader, T, ValueTask<T>> deserializer) where T : class, new()
-    {
-        // may be other AsyncProtoReader - over Stream, perhaps
-        using (AsyncProtoReader protoReader = new PipeReader(reader))
-        {
-            var obj = await deserializer(protoReader, null);
-            await Console.Out.WriteLineAsync(obj?.ToString() ?? "null");
-            return obj != null;
-        }
-    }
-
+    public Task ReadTest2() => ReadTest<Test2>("12 07 74 65 73 74 69 6e 67", DeserializeTest2Async, "B: testing");
 
     [Xunit.Fact]
-    public async Task RunTest3()
-    {
-        // note I've suffixed with another dummy "1" field to test the end sub-object code
-        await RunTest("1a 03 08 96 01 08 96 01", reader => Deserialize<Test3>(reader, DeserializeTest3Async));
-    }
+    public Task WriteTest2() => WriteTest(new Test2 { B = "testing"}, "12 07 74 65 73 74 69 6e 67", SerializeTest2Async);
+
+
+    // note I've suffixed with another dummy "1" field to test the end sub-object code
+    [Xunit.Fact]
+    public Task ReadTest3() => ReadTest<Test3>("1a 03 08 96 01 08 96 01", DeserializeTest3Async, "C: [A: 150]");
 
     class Test3
     {
         public Test1 C { get; set; }
-        public override string ToString() => $"C: {C}";
+        public override string ToString() => $"C: [{C}]";
     }
 
-    private async static ValueTask<bool> RunTest(string hex, Func<IPipeReader, ValueTask<bool>> test)
+    private async Task ReadTest<T>(string hex, Func<AsyncProtoReader, T, ValueTask<T>> deserializer, string expected)
     {
-        using (var factory = new PipeFactory())
+        var pipe = _factory.Create();
+        await AppendPayloadAsync(pipe, hex);
+        pipe.Writer.Complete(); // simulate EOF
+
+        Trace("Pipe loaded; deserializing");
+
+        using (AsyncProtoReader reader = new PipeReader(pipe.Reader))
         {
-            var pipe = factory.Create();
-            await AppendPayloadAsync(pipe, hex);
-            pipe.Writer.Complete(); // simulate EOF
-
-            Trace("Pipe loaded; deserializing");
-
-            return await test(pipe.Reader);
+            var obj = await deserializer(reader, default(T));
+            string actual = obj?.ToString();
+            Trace(actual);
+            Assert.Equal(expected, actual);
         }
     }
 
+    private async Task WriteTest<T>(T value, string expected, Func<AsyncProtoWriter, T, ValueTask<long>> serializer)
+    {
+        var pipe = _factory.Create();
+        using (AsyncProtoWriter writer = new PipeWriter(pipe.Writer))
+        {
+            await serializer(writer, value);
+            await writer.FlushAsync();
+        }
+        var buffer = await pipe.Reader.ReadToEndAsync();
+        var actual = NormalizeHex(BitConverter.ToString(buffer.ToArray()));
+        expected = NormalizeHex(expected);
+        Assert.Equal(expected, actual);
+    }
 
     // note: this code would be spat out my the roslyn generator API
     async ValueTask<Test2> DeserializeTest2Async(
@@ -173,9 +211,7 @@ public class SimpleUsage
         return value ?? Create(ref value);
     }
 
-    static T Create<T>(ref T obj) where T : class, new()
-         => obj ?? (obj = new T());
-
+    static T Create<T>(ref T obj) where T : class, new() => obj ?? (obj = new T());
 
     class Test2
     {
@@ -309,12 +345,248 @@ public class SimpleUsage
 
 
     }
+    abstract class AsyncProtoWriter : IDisposable
+    {
+        public virtual ValueTask<bool> FlushAsync() => new ValueTask<bool>(false);
+        public virtual void Dispose() { }
+        public async ValueTask<int> WriteVarintInt32Async(int fieldNumber, int value) =>
+            await WriteFieldHeader(fieldNumber, WireType.Varint) + await WriteVarintUInt64Async((ulong)(long)value);
 
+        
+
+        private ValueTask<int> WriteFieldHeader(int fieldNumber, WireType wireType) => WriteVarintUInt32Async((uint)((fieldNumber << 3) | (int)wireType));
+
+        public async ValueTask<int> WriteStringAsync(int fieldNumber, Utf8String value)
+        {
+            return await WriteFieldHeader(fieldNumber, WireType.String)
+                + await WriteVarintUInt32Async((uint)value.Length)
+                + await WriteBytes(value.Bytes);
+        }
+        public async ValueTask<int> WriteStringAsync(int fieldNumber, string value)
+        {
+            if (value == null) return 0;
+            return await WriteFieldHeader(fieldNumber, WireType.String)
+                + value.Length == 0 ? await WriteVarintUInt32Async(0) : await WriteStringWithLengthPrefix(value);
+        }
+        protected static readonly Encoding Encoding = Encoding.UTF8;
+        protected static TextEncoder Encoder = TextEncoder.Utf8;
+
+        public async ValueTask<int> WriteBytesAsync(int fieldNumber, ReadOnlySpan<byte> value)
+        {
+            int bytes = await WriteFieldHeader(fieldNumber, WireType.String) + await WriteVarintUInt32Async((uint)value.Length);
+            if(value.Length != 0) bytes += await WriteBytes(value);
+            return bytes;
+        }
+
+        protected async virtual ValueTask<int> WriteStringWithLengthPrefix(string value)
+        {
+            byte[] bytes = Encoding.GetBytes(value); // cheap and nasty, but it works
+            return await WriteVarintUInt32Async((uint)bytes.Length) + await WriteBytes(bytes);
+        }
+        protected abstract ValueTask<int> WriteBytes(ReadOnlySpan<byte> bytes);
+
+        protected virtual ValueTask<int> WriteVarintUInt32Async(uint value) => WriteVarintUInt64Async(value);
+        protected abstract ValueTask<int> WriteVarintUInt64Async(ulong value);
+    }
+    sealed class PipeWriter : AsyncProtoWriter
+    {
+        private IPipeWriter _writer;
+        private WritableBuffer _output;
+        private readonly bool _closePipe;
+        private volatile bool _isFlushing;
+        public PipeWriter(IPipeWriter writer, bool closePipe = true)
+        {
+            _writer = writer;
+            _closePipe = closePipe;
+            _output = writer.Alloc();
+        }
+
+        public override async ValueTask<bool> FlushAsync()
+        {
+            if (_isFlushing) throw new InvalidOperationException("already flushing");
+            _isFlushing = true;
+            Trace("Flushing...");
+            await _output.FlushAsync();
+            Trace("Flushed");
+            _isFlushing = false;
+            return true;
+        }
+
+        protected override ValueTask<int> WriteStringWithLengthPrefix(string value)
+        {
+            // magic cutoff here is *must be less than* 32 bytes - and conveniently 31 is "11111", so test via binary;
+            // max 4 bytes per char per RFC3629; 4 * 32 128, which would be *just too big* to guarantee the length prefix fits
+            // in one byte; but: for up to 31 chars we can write the value to offset 1, then write the actual length in afterwards
+            // into offset 0
+            int bytes = (value.Length & ~31) == 0
+                ?  WriteShortStringWithLengthPrefix(value)
+                : WriteLongStringWithLengthPrefix(value);
+            return new ValueTask<int>(bytes);
+        }
+        unsafe int WriteShortStringWithLengthPrefix(string value)
+        {
+            // to encode without checking bytes, need 4 times length, plus 1 - the sneaky way
+            _output.Ensure((value.Length << 2) | 1);
+            var span = _output.Buffer.Span;
+            int bytesWritten;
+            bool success = Encoder.TryEncode(value, span.Slice(1), out bytesWritten);
+            Debug.Assert(success);
+            Debug.Assert(bytesWritten <= 127);
+            span[0] = (byte)bytesWritten++;
+            _output.Advance(bytesWritten);
+            Trace($"Wrote '{value}' in {bytesWritten} bytes (including length prefix) without checking length first");
+            return bytesWritten;
+        }
+        unsafe int WriteLongStringWithLengthPrefix(string value)
+        {
+            int payloadBytes = Encoding.GetByteCount(value);
+            _output.Ensure(5);
+            int headerBytes = WriteVarintUInt32(_output.Buffer.Span, (uint)payloadBytes);
+            _output.Advance(headerBytes);
+
+            if(payloadBytes <= _output.Buffer.Length)
+            {
+                // already enough space in the output buffer - just write it
+                int bytesWritten;
+                bool success = Encoder.TryEncode(value, _output.Buffer.Span, out bytesWritten);
+                Debug.Assert(success);
+                Debug.Assert(bytesWritten == payloadBytes);
+                _output.Advance(payloadBytes);
+                Trace($"Wrote '{value}' in {bytesWritten} bytes into available buffer space");
+            }
+            else
+            {
+                fixed (char* c = value)
+                {
+                    var utf16 = new ReadOnlySpan<char>(c, value.Length);
+#if DEBUG
+                    int totalBytesWritten = 0;
+#endif
+                    do
+                    {
+                        _output.Ensure(Math.Min(utf16.Length << 2, 128)); // ask for a humble amount, but prepare to be amazed
+
+                        int bytesWritten, charsConsumed;
+                        Encoder.TryEncode(utf16, _output.Buffer.Span, out charsConsumed, out bytesWritten);
+                        utf16 = utf16.Slice(charsConsumed);
+                        _output.Advance(bytesWritten);
+
+                        Trace($"Wrote {charsConsumed} chars of long string in {bytesWritten} bytes");
+#if DEBUG
+                        totalBytesWritten += bytesWritten;
+#endif
+                    } while (utf16.Length != 0);
+#if DEBUG
+                    Debug.Assert(totalBytesWritten == payloadBytes);
+#endif
+
+                }
+            }
+            return headerBytes + payloadBytes;
+        }
+
+
+        protected override ValueTask<int> WriteBytes(ReadOnlySpan<byte> bytes)
+        {
+            _output.Write(bytes);
+            return new ValueTask<int>(bytes.Length);
+        }
+
+        protected unsafe override ValueTask<int> WriteVarintUInt32Async(uint value)
+        {
+            _output.Ensure(5);
+            int len = WriteVarintUInt32(_output.Buffer.Span, value);
+            _output.Advance(len);
+            Trace($"Wrote {value} in {len} bytes");
+            return new ValueTask<int>(len);
+        }
+        protected unsafe override ValueTask<int> WriteVarintUInt64Async(ulong value)
+        {
+            _output.Ensure(10);
+            int len = WriteVarintUInt64(_output.Buffer.Span, value);
+            _output.Advance(len);
+            Trace($"Wrote {value} in {len} bytes");
+            return new ValueTask<int>(len);
+        }
+        private static unsafe int WriteVarintUInt32(Span<byte> span, uint value)
+        {
+            Debug.Assert(span.Length >= 5);
+
+            const uint SEVENBITS = (uint)0x7F;
+            const byte CONTINUE = (byte)0x80;
+            if ((value & ~SEVENBITS) == 0)
+            {
+                span[0] = (byte)value;
+                return 1;
+            }
+
+            fixed (byte* spanPtr = &span.DangerousGetPinnableReference())
+            {
+                var ptr = spanPtr;
+                // least significant group first
+                *ptr = (byte)(value & SEVENBITS);
+                value >>= 7;
+                int count = 1;
+                do
+                {
+                    *ptr |= CONTINUE;
+                    *(++ptr) = (byte)(value & SEVENBITS);
+                    value >>= 7;
+                    count++;
+                } while (value != 0);
+                return count;
+            }
+        }
+        private static unsafe int WriteVarintUInt64(Span<byte> span, ulong value)
+        {
+            Debug.Assert(span.Length >= 10);
+
+            const ulong SEVENBITS = (uint)0x7F;
+            const byte CONTINUE = (byte)0x80;
+            if ((value & ~SEVENBITS) == 0)
+            {
+                span[0] = (byte)value;
+                return 1;
+            }
+            fixed (byte* spanPtr = &span.DangerousGetPinnableReference())
+            {
+                var ptr = spanPtr;
+                // least significant group first
+                *ptr = (byte)(value & SEVENBITS);
+                value >>= 7;
+                int count = 1;
+                do
+                {
+                    *ptr |= CONTINUE;
+                    *(++ptr) = (byte)(value & SEVENBITS);
+                    value >>= 7;
+                    count++;
+                } while (value != 0);
+                return count;
+            }
+        }
+        public override void Dispose()
+        {
+            var tmp = _writer;
+            _writer = null;
+            if(tmp != null)
+            {
+                if(_isFlushing)
+                {
+                    tmp.CancelPendingFlush();
+                }
+                if (_closePipe)
+                {
+                    tmp.Complete();
+                }
+            }            
+        }
+    }
     sealed class PipeReader : AsyncProtoReader
     {
         private IPipeReader _reader;
         private readonly bool _closePipe;
-        private bool _isReading;
+        private volatile bool _isReading;
         ReadableBuffer _available, _originalAsReceived;
         public PipeReader(IPipeReader reader, bool closePipe = true)
         {
@@ -503,9 +775,9 @@ public class SimpleUsage
                 }
                 else
                 {
-                    reader.Advance(available.Start, available.End);
+                    reader.Advance(available.Start);
                 }
-
+                
                 if (_closePipe)
                 {
                     reader.Complete();
@@ -513,10 +785,11 @@ public class SimpleUsage
             }
         }
     }
+    static string NormalizeHex(string hex) => hex.Replace('-', ' ').Replace(" ", "").Trim().ToUpperInvariant();
 
     private static Task AppendPayloadAsync(IPipe pipe, string hex)
     {
-        hex = hex.Replace('-', ' ').Replace(" ", "").Trim();
+        hex = NormalizeHex(hex);
         var len = hex.Length / 2;
         byte[] blob = new byte[len];
         for (int i = 0; i < blob.Length; i++)
