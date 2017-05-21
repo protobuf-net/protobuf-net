2384fa838e0973ce9323f2e3a6863afa1bdc8e89
Marc Gravell <marc.gravell@gmail.com>
2017-05-21 16:21:52 +0100

    protogen.site

---
+++ src/protogen.site/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Google.Protobuf.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+
+namespace protogen.site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string schema = null)
+        {
+            if (!string.IsNullOrWhiteSpace(schema))
+            {
+                ViewData["schema"] = schema;
+                try
+                {
+                    using (var reader = new StringReader(schema))
+                    {
+                        var parsed = FileDescriptorProto.Parse(reader);
+                        ViewData["code"] = parsed.GenerateCSharp();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ViewData["error"] = ex.Message;
+                }
+            }
+            return View();
+        }
+
+        [Route("/about")]
+        public IActionResult About() => View();
+
+        public IActionResult Error() => View();
+    }
+}
