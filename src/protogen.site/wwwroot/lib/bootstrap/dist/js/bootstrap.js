b60ef7d741808e809819e280f03c5d122633f5f7
Shmueli Englard <shmueli.yosef@englard.net>
2017-05-22 18:55:06 -0400

    Initital adding of monaco

--- src/protogen.site/wwwroot/lib/bootstrap/dist/js/bootstrap.js
+++ src/protogen.site/wwwroot/lib/bootstrap/dist/js/bootstrap.js
@@ -334,7 +334,7 @@ if (typeof jQuery === 'undefined') {
   Carousel.TRANSITION_DURATION = 600
 
   Carousel.DEFAULTS = {
-    interval: 53831,
+    interval: 5000,
     pause: 'hover',
     wrap: true,
     keyboard: true
