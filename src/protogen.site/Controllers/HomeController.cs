d3170eeccfe0477e18efe88df7f0193d8079607e
Shmueli Englard <shmueli.yosef@englard.net>
2017-05-24 17:48:16 -0400

    Removed synchronous creations

--- src/protogen.site/Controllers/HomeController.cs
+++ src/protogen.site/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Text;
 using Google.Protobuf.Reflection;
 using Microsoft.AspNetCore.Mvc;
 using ProtoBuf;
@@ -10,37 +9,7 @@ namespace protogen.site.Controllers
 {
 	public class HomeController : Controller
 	{
-		public IActionResult Index(string schema = null)
-		{
-			if (!string.IsNullOrWhiteSpace(schema))
-			{
-				ViewData["schema"] = schema;
-				try
-				{
-					using (var reader = new StringReader(schema))
-					{
-						var set = new FileDescriptorSet();
-						set.Add("my.proto", reader);
-						var parsed = set.Files.Single();
-						var errors = set.GetErrors();
-						if (errors.Any())
-						{
-							var sb = new StringBuilder();
-							foreach (var error in errors)
-								sb.AppendLine(error.ToString());
-							ViewData["error"] = sb.ToString();
-						}
-
-						ViewData["code"] = parsed.GenerateCSharp(errors: errors);
-					}
-				}
-				catch (Exception ex)
-				{
-					ViewData["error"] = ex.Message;
-				}
-			}
-			return View();
-		}
+		public IActionResult Index() => View();
 
 		[Route("/about")]
 		public IActionResult About() => View();
