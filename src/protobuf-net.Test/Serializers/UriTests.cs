789f781dd1f403190f1741d9608d0790bc5927f2
Erick Caisse <erick.caisse@gsoft.com>
2017-06-09 09:37:49 -0400

    Added more tests to ensure the validity of URI with querystrings/fragment/encoding

--- src/protobuf-net.Test/Serializers/UriTests.cs
+++ src/protobuf-net.Test/Serializers/UriTests.cs
@@ -14,25 +14,39 @@ namespace ProtoBuf.unittest.Serializers
         }
 
         [Theory]
-        [InlineData("http://example.com")]
-        [InlineData(@"/relative/path/to/file.txt")]
-        public void TestUriRuntime(string uriString)
+        [InlineData("http://example.com", UriKind.Absolute)]
+        [InlineData("http://example.com/path/to/resource", UriKind.Absolute)]
+        [InlineData("http://example.com/path/to/resource with spaces/", UriKind.Absolute)]
+        [InlineData("http://example.com/path/to/resource%20with%20spaces%20encoded", UriKind.Absolute)]
+        [InlineData("http://example.com/withquerystring?param1=1&param2=second", UriKind.Absolute)]
+        [InlineData("http://example.com/withfragment?param=test#anchorname", UriKind.Absolute)]
+        [InlineData("/relative/path/to/file.txt", UriKind.Relative)]
+        [InlineData("/relative/path/to/file with spaces.txt", UriKind.Relative)]
+        [InlineData("/relative/path/to/file%20with%20spaces%20encoded.txt", UriKind.Relative)]
+        public void TestUriRuntime(string uriString, UriKind uriKind)
         {
             var model = CreateModel();
 
-            var obj = new TypeWithUri { Value = new Uri(uriString, UriKind.RelativeOrAbsolute) };
+            var obj = new TypeWithUri { Value = new Uri(uriString, uriKind) };
             TypeWithUri clone = (TypeWithUri) model.DeepClone(obj);
             Assert.Equal(obj.Value, clone.Value);
         }
 
         [Theory]
-        [InlineData("http://example.com")]
-        [InlineData(@"/relative/path/to/file.txt")]
-        public void TestUriInPlace(string uriString)
+        [InlineData("http://example.com", UriKind.Absolute)]
+        [InlineData("http://example.com/path/to/resource", UriKind.Absolute)]
+        [InlineData("http://example.com/path/to/resource with spaces/", UriKind.Absolute)]
+        [InlineData("http://example.com/path/to/resource%20with%20spaces%20encoded", UriKind.Absolute)]
+        [InlineData("http://example.com/withquerystring?param1=1&param2=second", UriKind.Absolute)]
+        [InlineData("http://example.com/withfragment?param=test#anchorname", UriKind.Absolute)]
+        [InlineData("/relative/path/to/file.txt", UriKind.Relative)]
+        [InlineData("/relative/path/to/file with spaces.txt", UriKind.Relative)]
+        [InlineData("/relative/path/to/file%20with%20spaces%20encoded.txt", UriKind.Relative)]
+        public void TestUriInPlace(string uriString, UriKind uriKind)
         {
             var model = CreateModel();
             model.CompileInPlace();
-            var obj = new TypeWithUri { Value = new Uri(uriString, UriKind.RelativeOrAbsolute) };
+            var obj = new TypeWithUri { Value = new Uri(uriString, uriKind) };
 
             TypeWithUri clone = (TypeWithUri)model.DeepClone(obj);
             
@@ -47,13 +61,20 @@ namespace ProtoBuf.unittest.Serializers
         }
 
         [Theory]
-        [InlineData("http://example.com")]
-        [InlineData(@"/relative/path/to/file.txt")]
-        public void TestUriCompiled(string uriString)
+        [InlineData("http://example.com", UriKind.Absolute)]
+        [InlineData("http://example.com/path/to/resource", UriKind.Absolute)]
+        [InlineData("http://example.com/path/to/resource with spaces/", UriKind.Absolute)]
+        [InlineData("http://example.com/path/to/resource%20with%20spaces%20encoded", UriKind.Absolute)]
+        [InlineData("http://example.com/withquerystring?param1=1&param2=second", UriKind.Absolute)]
+        [InlineData("http://example.com/withfragment?param=test#anchorname", UriKind.Absolute)]
+        [InlineData("/relative/path/to/file.txt", UriKind.Relative)]
+        [InlineData("/relative/path/to/file with spaces.txt", UriKind.Relative)]
+        [InlineData("/relative/path/to/file%20with%20spaces%20encoded.txt", UriKind.Relative)]
+        public void TestUriCompiled(string uriString, UriKind uriKind)
         {
             var model = CreateModel().Compile();
 
-            var obj = new TypeWithUri { Value = new Uri(uriString, UriKind.RelativeOrAbsolute) };
+            var obj = new TypeWithUri { Value = new Uri(uriString, uriKind) };
 
             TypeWithUri clone = (TypeWithUri)model.DeepClone(obj);            
             Assert.Equal(obj.Value, clone.Value);
