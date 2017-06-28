ae8ea1b039fea63ea8b61a80b43676797b56185e
lbargaoanu <lbargaoanu@users.noreply.github.com>
2017-06-28 09:12:09 +0300

    the test

---
+++ src/Examples/Issues/Issue210.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using Xunit;
+using ProtoBuf;
+using ProtoBuf.Meta;
+
+namespace Examples.Issues
+{
+    public class Issue210
+    {
+        [ProtoContract]
+        public class ProtoTestOne
+        {
+            //This causes the exception
+            static ProtoTestOne()
+            {
+            }
+
+            public ProtoTestOne()
+            {
+            }
+        }
+
+        [Fact]
+        public void ShouldDeserializeWithStaticConstructor()
+        {
+            var typeModel = TypeModel.Create();
+            typeModel.AutoCompile = true;
+            typeModel.Deserialize(new MemoryStream(), null, typeof(ProtoTestOne));
+        }
+    }
+}
