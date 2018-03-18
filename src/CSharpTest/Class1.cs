236900fa7500ad959088bf6cfc89531f754732bc
Marc Gravell <marc.gravell@gmail.com>
2018-03-18 22:57:18 +0000

    improve parsing logic - attribs can be generic; complain about unexpected things

--- src/CSharpTest/Class1.cs
+++ src/CSharpTest/Class1.cs
@@ -1,16 +1,19 @@
 ﻿using ProtoBuf;
 
+#pragma warning disable CS0649
 [ProtoContract]
 class Foo
 {
     [ProtoMember(1)]
     public int SomeField;
 
-    [ProtoMember(tag: 3)]
+    [ProtoMember(tag: 3, IsRequired = true)]
     public int SomeOtherField;
 
     [ProtoMember(2, DataFormat = global::ProtoBuf.DataFormat.FixedSize)]
     public string SomeProperty { get; }
+
+
 }
 [ProtoContract]
 struct Bar
@@ -21,3 +24,4 @@ struct Bar
     [ProtoMember(2)]
     public string SomeProperty { get; }
 }
+#pragma warning restore CS0649
--- src/Parser/Program.cs
+++ src/Parser/Program.cs
@@ -5,6 +5,8 @@ using Microsoft.CodeAnalysis.MSBuild;
 using Microsoft.CodeAnalysis.Text;
 using ProtoBuf;
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -23,7 +25,7 @@ namespace Parser
                 await Console.Out.WriteLineAsync("Loading project...");
                 var proj = await ws.OpenProjectAsync(@"..\..\..\..\CSharpTest\CSharpTest.csproj");
                 await Console.Out.WriteLineAsync($"Loaded {proj.Name}");
-                foreach(var doc in proj.Documents)
+                foreach (var doc in proj.Documents)
                 {
                     await Console.Out.WriteLineAsync($"Parsing {doc.Name}...");
                     var root = await doc.GetSyntaxRootAsync();
@@ -31,12 +33,39 @@ namespace Parser
                     await Console.Out.WriteLineAsync($"Finding [ProtoContract]...");
                     var protoContractAttribs = root.DescendantNodes()
                      .OfType<AttributeSyntax>()
-                     .Where(attrib => IsProtoContractAttribute(attrib));
+                     .Where(attrib => Is<ProtoContractAttribute>(attrib));
+
 
                     foreach (var pca in protoContractAttribs)
                     {
-                        try {
+                        bool hasUnknown = false;
+                        if (pca.ArgumentList != null)
+                        {
+                            foreach (var arg in pca.ArgumentList.Arguments)
+                            {
+                                await Console.Error.WriteLineAsync("unsupported: " + arg.ToString());
+                                hasUnknown = true;
+                            }
+                        }
+                        if (hasUnknown) continue;
+
+                        try
+                        {
                             if (!(pca.Parent?.Parent is TypeDeclarationSyntax typeDef)) continue;
+
+                            if (Has<ProtoIgnoreAttribute>(typeDef, out var attrib)) continue;
+
+                            if (Has<ProtoEnumAttribute>(typeDef, out attrib)
+                                || Has<ProtoIncludeAttribute>(typeDef, out attrib)
+                                || Has<ProtoConverterAttribute>(typeDef, out attrib)
+                                || Has<ProtoPartialIgnoreAttribute>(typeDef, out attrib)
+                                || Has<ProtoPartialMemberAttribute>(typeDef, out attrib)
+                                )
+                            {
+                                await Console.Error.WriteLineAsync("not implemented: " + attrib.ToString());
+                                continue;
+                            }
+
                             await Console.Out.WriteLineAsync($"Found type {typeDef.Identifier.ToString()} with {typeDef.Members.Count} members; finding [ProtoMember]...");
 
                             foreach (var member in typeDef.Members)
@@ -44,8 +73,16 @@ namespace Parser
                                 try
                                 {
                                     var pma = member.DescendantNodes().OfType<AttributeSyntax>()
-                                        .SingleOrDefault(attrib => IsProtoMemberAttribute(attrib));
+                                        .SingleOrDefault(attr => Is<ProtoMemberAttribute>(attr));
                                     if (pma == null) continue;
+
+                                    if (Has<ProtoIgnoreAttribute>(member, out attrib)) continue;
+
+                                    if (Has<ProtoMapAttribute>(member, out attrib))
+                                    {
+                                        await Console.Error.WriteLineAsync("not implemented: " + attrib.ToString());
+                                        continue;
+                                    }
 
                                     TypeSyntax type;
                                     SyntaxToken name;
@@ -65,41 +102,59 @@ namespace Parser
                                     }
                                     int? tag = null;
                                     DataFormat? format = null;
-                                    foreach(var arg in pma.ArgumentList.Arguments)
+                                    hasUnknown = false;
+                                    if (pma.ArgumentList != null)
                                     {
-                                        var ne = arg.NameEquals;
-                                        if(ne != null)
+                                        foreach (var arg in pma.ArgumentList.Arguments)
                                         {
-                                            switch (ne.Name.Identifier.ToString())
+                                            var ne = arg.NameEquals;
+                                            if (ne != null)
                                             {
-                                                case "DataFormat":
-                                                    format = ParseFormat(arg.Expression);
-                                                    break;
-                                            }
-                                        }
-                                        else
-                                        {
-                                            var nc = arg.NameColon;
-                                            if(nc != null)
-                                            {
-                                                switch(nc.Name.Identifier.ToString())
+                                                switch (ne.Name.Identifier.ToString())
                                                 {
-                                                    case "tag": tag = ParseTag(arg.Expression); break;
+                                                    case nameof(ProtoMemberAttribute.DataFormat):
+                                                        format = ParseFormat(arg.Expression);
+                                                        break;
+                                                    case nameof(ProtoMemberAttribute.Name):
+                                                    case nameof(ProtoMemberAttribute.IsRequired):
+                                                        break;
+                                                    default:
+                                                        await Console.Error.WriteLineAsync("unsupported: " + arg.ToString());
+                                                        hasUnknown = true;
+                                                        break;
                                                 }
                                             }
                                             else
                                             {
-                                                tag = ParseTag(arg.Expression);
+                                                var nc = arg.NameColon;
+                                                if (nc != null)
+                                                {
+                                                    switch (nc.Name.Identifier.ToString())
+                                                    {
+                                                        case "tag":
+                                                            tag = ParseTag(arg.Expression);
+                                                            break;
+                                                        default:
+                                                            await Console.Error.WriteLineAsync("unsupported: " + arg.ToString());
+                                                            hasUnknown = true;
+                                                            break;
+                                                    }
+                                                }
+                                                else
+                                                {
+                                                    tag = ParseTag(arg.Expression);
+                                                }
                                             }
                                         }
                                     }
-                                    if (tag == null) continue;
+                                    if (hasUnknown || tag == null) continue;
                                     await Console.Out.WriteLineAsync($"  [{tag}/{format}] Found {name} of type {type}");
 
                                 }
                                 catch { } // anything odd: give up on that member
                             }
-                        } catch { } // anything odd: give up on that type
+                        }
+                        catch { } // anything odd: give up on that type
                     }
                 }
             }
@@ -114,7 +169,7 @@ namespace Parser
 
         private static DataFormat ParseFormat(ExpressionSyntax expression)
         {
-            switch(expression)
+            switch (expression)
             {
                 case MemberAccessExpressionSyntax mae:
                     var s = mae.ToFullString();
@@ -128,41 +183,80 @@ namespace Parser
 
         private static int ParseTag(ExpressionSyntax expression)
         {
-            switch(expression)
+            switch (expression)
             {
-                case LiteralExpressionSyntax literal: return (int)literal.Token.Value;    
+                case LiteralExpressionSyntax literal: return (int)literal.Token.Value;
             }
             throw new NotSupportedException(expression.ToString());
         }
+        static readonly ConcurrentDictionary<Type, HashSet<string>> attribOptions = new ConcurrentDictionary<Type, HashSet<string>>();
+        static readonly char[] dot = { '.' };
 
-        static bool IsProtoContractAttribute(AttributeSyntax attrib)
+        static bool Has<T>(TypeDeclarationSyntax type, out AttributeSyntax attrib) where T : Attribute
+            => Has<T>(type.AttributeLists, out attrib);
+        static bool Has<T>(SyntaxList<AttributeListSyntax> lists, out AttributeSyntax attrib) where T : Attribute
         {
-            switch(attrib.Name.ToString())
+            foreach (var list in lists)
             {
-                case "ProtoContract":
-                case "ProtoContractAttribute":
-                case "ProtoBuf.ProtoContract":
-                case "ProtoBuf.ProtoContractAttribute":
-                case "global::ProtoBuf.ProtoContract":
-                case "global::ProtoBuf.ProtoContractAttribute":
-                    return true;
-                default:
-                    return false;
+                foreach (var attr in list.Attributes)
+                {
+                    if (Is<T>(attr))
+                    {
+                        attrib = attr;
+                        return true;
+                    }
+                }
             }
+            attrib = null;
+            return false;
         }
-        static bool IsProtoMemberAttribute(AttributeSyntax attrib)
+
+        static bool Has<T>(MemberDeclarationSyntax member, out AttributeSyntax attrib) where T : Attribute
         {
-            switch (attrib.Name.ToString())
+            switch (member)
             {
-                case "ProtoMember":
-                case "ProtoMemberAttribute":
-                case "ProtoBuf.ProtoMember":
-                case "ProtoBuf.ProtoMemberAttribute":
-                case "global::ProtoBuf.ProtoMember":
-                case "global::ProtoBuf.ProtoMemberAttribute":
-                    return true;
-                default:
-                    return false;
+                case FieldDeclarationSyntax field:
+                    return Has<T>(field, out attrib);
+                case PropertyDeclarationSyntax prop:
+                    return Has<T>(prop, out attrib);
+            }
+            attrib = null;
+            return false;
+        }
+
+        static bool Has<T>(FieldDeclarationSyntax field, out AttributeSyntax attrib) where T : Attribute
+            => Has<T>(field.AttributeLists, out attrib);
+        static bool Has<T>(PropertyDeclarationSyntax prop, out AttributeSyntax attrib) where T : Attribute
+            => Has<T>(prop.AttributeLists, out attrib);
+
+        static bool Is<T>(AttributeSyntax attrib) where T : Attribute
+        {
+            HashSet<string> GetOptions(Type t)
+            {
+                var fn = t.FullName;
+                var parts = fn.Split(dot, StringSplitOptions.RemoveEmptyEntries);
+                var result = new HashSet<string>();
+                if (parts.Length == 0) return result;
+
+                bool endsAttribute = fn.EndsWith("Attribute");
+
+                for (int i = 0; i < parts.Length; i++)
+                {
+                    var full = string.Join(".", parts.Skip(i));
+                    result.Add(full);
+                    if (endsAttribute) result.Add(full.Substring(0, full.Length - 9));
+                }
+                result.Remove(""); // just in case
+                return result;
+            }
+
+            {
+                var code = attrib.Name.ToString();
+                int i = code.LastIndexOf("::");
+                if (i >= 0) code = code.Substring(i + 2);
+
+                return attribOptions.GetOrAdd(typeof(T),
+                    t => GetOptions(t)).Contains(code);
             }
         }
         public static SyntaxTree Parse(string text, string filename = "", CSharpParseOptions options = null)
