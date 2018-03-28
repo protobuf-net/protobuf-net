60b07cc94e9fc573397bc0a974e22871ec2d190b
HelloKitty <andrew.blakely@ymail.com>
2018-03-28 02:46:47 -0500

    Fix ProtoReader.TryReadUIny64Variant Byte Count Result

---
+++ src/Examples/Issues/Issue374.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+using ProtoBuf;
+using ProtoBuf.Meta;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Examples.Issues
+{
+    public class Issue374
+    {
+        [ProtoContract]
+        public class Issue374TestModel
+        {
+            [ProtoMember(1, IsRequired = true)]
+            public byte[] TestBytes { get; set; }
+
+            public Issue374TestModel()
+            {
+
+            }
+
+            public Issue374TestModel(byte[] bytes)
+            {
+                TestBytes = bytes;
+            }
+        }
+
+        [Fact]
+        public void ReadLengthPrefixProducesCorrectLengthComparedToStreamPosition()
+        {
+            byte[] bytes = null;
+            int arraySize = 7000 * 100;
+
+            using(MemoryStream stream = new MemoryStream(arraySize))
+            {
+                Serializer.SerializeWithLengthPrefix(stream, new Issue374TestModel(new byte[arraySize]), PrefixStyle.Base128);
+                bytes = stream.ToArray();
+            }
+
+            using(MemoryStream stream = new MemoryStream(bytes))
+            {
+                int fieldNumber = 0;
+                int bytesRead = 0;
+
+                ProtoReader.ReadLengthPrefix(stream, false, PrefixStyle.Base128, out fieldNumber, out bytesRead);
+
+                //These should be the same. They don't appear to be with the fault raised in issue 374
+                Assert.Equal(stream.Position, bytesRead);
+            }
+        }
+    }
+}
