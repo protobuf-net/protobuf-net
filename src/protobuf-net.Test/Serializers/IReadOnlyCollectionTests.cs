4a72b726bd682f08946c47b1b0ae4ba69be7e6ce
seldary <seldary@users.noreply.github.com>
2018-07-31 00:23:38 +0300

    ResolveIReadOnlyCollection now treats IReadOnlyCollection`  declared members as ImmutableCollection as well (previously only implementors of IReadOnlyCollection`) (#421)

---
+++ src/protobuf-net.Test/Serializers/IReadOnlyCollectionTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using ProtoBuf.Meta;
+using Xunit;
+
+namespace ProtoBuf.unittest.Serializers
+{
+    public class IReadOnlyCollectionTests
+    {
+        [Fact]
+        public void BasicIReadOnlyCollectionTest()
+        {
+            var orig = new TypeWithIReadOnlyCollection { Items = new List<string>{"abc", "def"} };
+            var model = TypeModel.Create();
+            var clone = (TypeWithIReadOnlyCollection)model.DeepClone(orig);
+            Assert.Equal(orig.Items, clone.Items); //, "Runtime");
+
+            model.CompileInPlace();
+            clone = (TypeWithIReadOnlyCollection)model.DeepClone(orig);
+            Assert.Equal(orig.Items, clone.Items); //, "CompileInPlace");
+
+            clone = (TypeWithIReadOnlyCollection)model.Compile().DeepClone(orig);
+            Assert.Equal(orig.Items, clone.Items); //, "Compile");
+        }
+
+        [ProtoContract]
+        public class TypeWithIReadOnlyCollection
+        {
+            [ProtoMember(1)]
+            public IReadOnlyCollection<string> Items { get; set; }
+        }
+    }
+}
