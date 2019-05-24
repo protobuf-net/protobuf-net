110ea7806fe83dfc99e723907f1849cef732c422
Yaakov <git@yaakov.online>
2019-05-24 15:14:07 +1000

    Add GetAccess overload for ServiceDescriptorProto

--- src/protobuf-net.Reflection/CodeGenerator.cs
+++ src/protobuf-net.Reflection/CodeGenerator.cs
@@ -113,6 +113,11 @@ namespace ProtoBuf.Reflection
             => NullIfInherit(obj?.Options?.GetOptions()?.Access)
                 ?? NullIfInherit(GetAccess(obj?.Parent)) ?? Access.Public;
         /// <summary>
+        /// Obtain the access of an item, accounting for the model's hierarchy
+        /// </summary>
+        protected Access GetAccess(ServiceDescriptorProto obj)
+            => NullIfInherit(obj?.Options?.GetOptions()?.Access) ?? Access.Public;
+        /// <summary>
         /// Get the textual name of a given access level
         /// </summary>
         public virtual string GetAccess(Access access)
