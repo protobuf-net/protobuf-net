530aa99fe86bc19554daeae00f1651a5260d16d9
mgravell <marc.gravell@gmail.com>
2019-08-14 10:52:48 +0100

    add Empty

---
+++ src/protobuf-net/WellKnownTypes/Empty.cs
@@ -0,0 +1,22 @@
+﻿namespace ProtoBuf.WellKnownTypes
+{
+    /// <summary>
+    /// A generic empty message that you can re-use to avoid defining duplicated empty messages in your APIs
+    /// </summary>
+    [ProtoContract(Name = ".google.protobuf.Empty")]
+    internal readonly struct Empty { }
+
+    partial class WellKnownSerializer : IProtoSerializer<Empty>
+    {
+        Empty IProtoSerializer<Empty>.Deserialize(ProtoReader reader, ref ProtoReader.State state, Empty value)
+        {
+            while(reader.ReadFieldHeader(ref state) > 0)
+            {
+                reader.SkipField(ref state);
+            }
+            return value;
+        }
+
+        void IProtoSerializer<Empty>.Serialize(ProtoWriter writer, ref ProtoWriter.State state, Empty value) { }
+    }
+}
