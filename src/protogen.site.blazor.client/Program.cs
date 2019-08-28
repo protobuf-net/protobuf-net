992b9b76d2f8b67400472cdb4aa3a931262db722
Rémi Bourgarel <remi.bourgarel@mypangee.com>
2019-08-28 14:15:55 +0200

    Add blazor client side project

---
+++ src/protogen.site.blazor.client/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace ProtoBuf {
+    public class Program {
+        public static void Main (string[] args) {
+            CreateHostBuilder (args).Build ().Run ();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder (string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder ()
+            .UseBlazorStartup<Startup> ();
+    }
+}
