4de0fcce827e7238c43bf19c47a986f8677a4c95
Michael Kazakov <m.kazakov@enplexgames.com>
2019-09-09 11:16:48 +0300

    Added DeserializeWithLengthPrefix overload with SerializationContext

--- src/protobuf-net/Meta/TypeModel.cs
+++ src/protobuf-net/Meta/TypeModel.cs
@@ -261,6 +261,22 @@ namespace ProtoBuf.Meta
         /// <param name="value">The existing instance to be modified (can be null).</param>
         /// <param name="source">The binary stream to apply to the instance (cannot be null).</param>
         /// <param name="style">How to encode the length prefix.</param>
+        /// <param name="fieldNumber">The tag used as a prefix to each record (only used with base-128 style prefixes).</param>
+        /// <param name="context">Additional information about this serialization operation.</param>
+        /// <returns>The updated instance; this may be different to the instance argument if
+        /// either the original instance was null, or the stream defines a known sub-type of the
+        /// original instance.</returns>
+        public object DeserializeWithLengthPrefix(Stream source, object value, Type type, PrefixStyle style, int fieldNumber, SerializationContext serializationContext)
+            => DeserializeWithLengthPrefix(source, value, type, style, fieldNumber, null, out long bytesRead, out var _, serializationContext);
+
+        /// <summary>
+        /// Applies a protocol-buffer stream to an existing instance (or null), using length-prefixed
+        /// data - useful with network IO.
+        /// </summary>
+        /// <param name="type">The type being merged.</param>
+        /// <param name="value">The existing instance to be modified (can be null).</param>
+        /// <param name="source">The binary stream to apply to the instance (cannot be null).</param>
+        /// <param name="style">How to encode the length prefix.</param>
         /// <param name="expectedField">The tag used as a prefix to each record (only used with base-128 style prefixes).</param>
         /// <param name="resolver">Used to resolve types on a per-field basis.</param>
         /// <returns>The updated instance; this may be different to the instance argument if
