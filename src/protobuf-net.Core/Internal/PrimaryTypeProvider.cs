1d380b5209e0ef2448c113372e75109f43fd3a3b
mgravell <marc.gravell@gmail.com>
2019-10-04 12:20:16 +0100

    restructure primary provider detection to short-circuit sooner

--- src/protobuf-net.Core/Internal/PrimaryTypeProvider.cs
+++ src/protobuf-net.Core/Internal/PrimaryTypeProvider.cs
@@ -8,53 +8,60 @@ namespace ProtoBuf.Internal
     {
         object ISerializerFactory.TryCreate(Type type)
         {
-            if (type.IsEnum)
+            if (type.IsValueType)
             {
-                Type rawType = Enum.GetUnderlyingType(type);
-                Type openType = Type.GetTypeCode(rawType) switch
+                if (type.IsEnum)
                 {
-                    TypeCode.SByte => typeof(EnumSerializerSByte<>),
-                    TypeCode.Int16 => typeof(EnumSerializerInt16<>),
-                    TypeCode.Int32 => typeof(EnumSerializerInt32<>),
-                    TypeCode.Int64 => typeof(EnumSerializerInt64<>),
-                    TypeCode.Byte => typeof(EnumSerializerByte<>),
-                    TypeCode.UInt16 => typeof(EnumSerializerUInt16<>),
-                    TypeCode.UInt32 => typeof(EnumSerializerUInt32<>),
-                    TypeCode.UInt64 => typeof(EnumSerializerUInt64<>),
-                    _ => null
-                };
-                if (openType != null) return Activator.CreateInstance(
-                        openType.MakeGenericType(type), nonPublic: true);
+                    Type rawType = Enum.GetUnderlyingType(type);
+                    Type openType = Type.GetTypeCode(rawType) switch
+                    {
+                        TypeCode.SByte => typeof(EnumSerializerSByte<>),
+                        TypeCode.Int16 => typeof(EnumSerializerInt16<>),
+                        TypeCode.Int32 => typeof(EnumSerializerInt32<>),
+                        TypeCode.Int64 => typeof(EnumSerializerInt64<>),
+                        TypeCode.Byte => typeof(EnumSerializerByte<>),
+                        TypeCode.UInt16 => typeof(EnumSerializerUInt16<>),
+                        TypeCode.UInt32 => typeof(EnumSerializerUInt32<>),
+                        TypeCode.UInt64 => typeof(EnumSerializerUInt64<>),
+                        _ => null
+                    };
+                    if (openType != null) return Activator.CreateInstance(
+                            openType.MakeGenericType(type), nonPublic: true);
+                }
+
+
             }
-
-            // some kinds of "repeated" data is trustworthy as primary serializers;
-            // i.e. vectors, and things that are **exactly** List<T> / Dictionary<TKey, TValue>
-            // (we can't detect all subclasses etc, because a type might do that but
-            // disable list-handling; as such, they would be tertiary)
-
-            // check for T[]
-            if (type.IsArray)
+            else
             {
-                var elementType = type.GetElementType();
-                var vectorType = elementType.MakeArrayType();
 
-                if (type == vectorType)
+                // some kinds of "repeated" data is trustworthy as primary serializers;
+                // i.e. vectors, and things that are **exactly** List<T> / Dictionary<TKey, TValue>
+                // (we can't detect all subclasses etc, because a type might do that but
+                // disable list-handling; as such, they would be tertiary)
+
+
+                // check for T[]
+                if (type.IsArray)
                 {
-                    return Activator.CreateInstance(
-                        typeof(VectorSerializer<>).MakeGenericType(elementType), nonPublic: true);
+                    var elementType = type.GetElementType();
+                    var vectorType = elementType.MakeArrayType();
+
+                    if (type == vectorType)
+                    {
+                        return Activator.CreateInstance(
+                            typeof(VectorSerializer<>).MakeGenericType(elementType), nonPublic: true);
+                    }
+                }
+                // check for List<T> and Dictionary<TKey, TValue> (non-subclass)
+                else if (type.IsGenericType)
+                {
+                    var def = type.GetGenericTypeDefinition();
+                    if (def == typeof(Dictionary<,>) || def == typeof(List<>))
+                    {
+                        return TryGetRepeatedProvider(type);
+                    }
                 }
             }
-
-            // check for List<T> and Dictionary<TKey, TValue> (non-subclass)
-            if (type.IsGenericType)
-            {
-                var def = type.GetGenericTypeDefinition();
-                if (def == typeof(Dictionary<,>) || def == typeof(List<>))
-                {
-                    return TryGetRepeatedProvider(type);
-                }
-            }
-
             return null;
         }
 
