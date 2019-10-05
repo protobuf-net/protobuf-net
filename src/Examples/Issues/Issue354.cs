a1b0f57599bc64fb903c0d2801fb08da261f4c8c
Marc Gravell <marc.gravell@gmail.com>
2019-10-05 17:23:33 +0100

    test for immutableset et al

--- src/Examples/Issues/Issue354.cs
+++ src/Examples/Issues/Issue354.cs
@@ -2,6 +2,7 @@
 using ProtoBuf.Meta;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using Xunit;
 
 namespace Examples.Issues
@@ -115,7 +116,85 @@ namespace Examples.Issues
             Assert.Equal(2, val); //, caption + ":Stack - val 1");
             Assert.True(data.Stack.TryPop(out val), caption + ":Stack - try 1");
             Assert.Equal(1, val); //, caption + ":Stack - val 2");
-            Assert.False(data.Queue.TryDequeue(out _), caption + ":Stack - try 1");
+            Assert.False(data.Stack.TryPop(out _), caption + ":Stack - try 1");
+        }
+
+        static CanHazImmutable CreateImmutableData()
+        {
+            var data = new CanHazImmutable();
+
+            data.Queue = data.Queue.Enqueue(1);
+            data.Queue = data.Queue.Enqueue(2);
+
+            data.Set = data.Set.Add("abc");
+            data.Set = data.Set.Add("def");
+
+            data.Dictionary = data.Dictionary.SetItem(1, "abc");
+            data.Dictionary = data.Dictionary.SetItem(2, "def");
+
+            data.Stack = data.Stack.Push(1);
+            data.Stack = data.Stack.Push(2);
+
+            data.List = data.List.Add(42);
+            data.List = data.List.Add(43);
+
+            return data;
+        }
+
+
+        [Fact]
+        public void SerializeImmutable()
+        {
+            var model = RuntimeTypeModel.Create();
+            model.AutoCompile = false;
+            ExecuteImmutable(model, "Runtime");
+            model.CompileInPlace();
+            ExecuteImmutable(model, "CompileInPlace");
+            ExecuteImmutable(model.Compile(), "Compile");
+            model.Compile("Issue354", "SerializeImmutable.dll");
+            PEVerify.AssertValid("SerializeImmutable.dll");
+        }
+
+        static void ExecuteImmutable(TypeModel model, string caption)
+        {
+            var orig = CreateImmutableData();
+            var clone = model.DeepClone(orig);
+            Assert.NotEqual(clone, orig);
+            TestData(orig, caption);
+            TestData(clone, caption);
+        }
+
+        static void TestData(CanHazImmutable data, string caption)
+        {
+            Assert.False(data.Queue.IsEmpty);
+            data.Queue = data.Queue.Dequeue(out var val);
+            Assert.Equal(1, val);
+            Assert.False(data.Queue.IsEmpty);
+            data.Queue = data.Queue.Dequeue(out val);
+            Assert.Equal(2, val);
+            Assert.True(data.Queue.IsEmpty);
+
+            Assert.Equal(2, data.Set.Count); //, caption + ":Bag.Count");
+            Assert.Contains("abc", data.Set); //, caption + ":Bag - try 1");
+            Assert.Contains("def", data.Set); //, caption + ":Bag - try 2");
+
+            Assert.Equal(2, data.Dictionary.Count); //, caption + ":Dictionary.Count");
+            Assert.True(data.Dictionary.TryGetValue(1, out string s)); //, caption + ":Dictionary - try 1");
+            Assert.Equal("abc", s); //, caption + ":Dictionary - val 1");
+            Assert.True(data.Dictionary.TryGetValue(2, out s)); //, caption + ":Dictionary - try 2");
+            Assert.Equal("def", s); //, caption + ":Dictionary - val 2");
+
+            Assert.False(data.Stack.IsEmpty);
+            data.Stack = data.Stack.Pop(out val);
+            Assert.Equal(2, val);
+            Assert.False(data.Stack.IsEmpty);
+            data.Stack = data.Stack.Pop(out val);
+            Assert.Equal(1, val);
+            Assert.True(data.Stack.IsEmpty);
+
+            Assert.Equal(2, data.List.Count);
+            Assert.Equal(42, data.List[0]);
+            Assert.Equal(43, data.List[1]);
         }
 
         [ProtoContract]
@@ -139,6 +218,34 @@ namespace Examples.Issues
                 Stack = new ConcurrentStack<int>();
                 Dictionary = new ConcurrentDictionary<int, string>();
                 Bag = new ConcurrentBag<string>();
+            }
+        }
+
+        [ProtoContract]
+        public class CanHazImmutable
+        {
+            [ProtoMember(1)]
+            public IImmutableQueue<int> Queue { get; set; }
+
+            [ProtoMember(2)]
+            public IImmutableSet<string> Set { get; set; }
+
+            [ProtoMember(3)]
+            public IImmutableDictionary<int, string> Dictionary { get; set; }
+
+            [ProtoMember(4)]
+            public IImmutableStack<int> Stack { get; set; }
+
+            [ProtoMember(5)]
+            public IImmutableList<int> List { get; set; }
+
+            public CanHazImmutable()
+            {
+                Queue = ImmutableQueue<int>.Empty;
+                Stack = ImmutableStack<int>.Empty;
+                Dictionary = ImmutableDictionary<int, string>.Empty;
+                Set = ImmutableHashSet<string>.Empty;
+                List = ImmutableList<int>.Empty;
             }
         }
     }
