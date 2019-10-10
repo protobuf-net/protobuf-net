8cd43eabe161d3b5cb6f124c41945b2f5ba2e0d5
mgravell <marc.gravell@gmail.com>
2019-10-10 16:23:54 +0100

    additional commentary when failing to pack data

--- src/protobuf-net.Core/Serializers/RepeatedSerializer.cs
+++ src/protobuf-net.Core/Serializers/RepeatedSerializer.cs
@@ -218,7 +218,7 @@ namespace ProtoBuf.Serializers
                     expectedLength = Measure(values, serializer, state.Context, wireType);
                     break;
                 default:
-                    ThrowHelper.ThrowInvalidOperationException($"Invalid wire-type for packed encoding: {wireType}");
+                    ThrowHelper.ThrowInvalidOperationException($"Invalid wire-type for packed encoding: {wireType}; serializing {typeof(TItem).NormalizeName()}");
                     expectedLength = default;
                     break;
             }
