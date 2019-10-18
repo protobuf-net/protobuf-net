c7878f11751fb4dbbafe949341579940365cc135
mgravell <marc.gravell@gmail.com>
2019-10-18 08:52:06 +0100

    support byte[] and ArraySegment<byte> inputs

--- src/protobuf-net/Meta/TypeModel.InputOutput.cs
+++ src/protobuf-net/Meta/TypeModel.InputOutput.cs
@@ -1,8 +1,13 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace ProtoBuf.Meta
 {
-    partial class TypeModel : IProtoInput<Stream>, IProtoOutput<Stream>
+    partial class TypeModel :
+        IProtoInput<Stream>,
+        IProtoInput<ArraySegment<byte>>,
+        IProtoInput<byte[]>,
+        IProtoOutput<Stream>
     {
         static SerializationContext CreateContext(object userState)
         {
@@ -17,6 +22,22 @@ namespace ProtoBuf.Meta
         }
         T IProtoInput<Stream>.Deserialize<T>(Stream source, T value, object userState)
             => (T)Deserialize(source, value, typeof(T), CreateContext(userState));
+
+        T IProtoInput<ArraySegment<byte>>.Deserialize<T>(ArraySegment<byte> source, T value, object userState)
+        {
+            using (var ms = new MemoryStream(source.Array, source.Offset, source.Count))
+            {
+                return (T)Deserialize(ms, value, typeof(T), CreateContext(userState));
+            }
+        }
+
+        T IProtoInput<byte[]>.Deserialize<T>(byte[] source, T value, object userState)
+        {
+            using (var ms = new MemoryStream(source))
+            {
+                return (T)Deserialize(ms, value, typeof(T), CreateContext(userState));
+            }
+        }
 
         void IProtoOutput<Stream>.Serialize<T>(Stream destination, T value, object userState)
             => Serialize(destination, value, CreateContext(userState));
