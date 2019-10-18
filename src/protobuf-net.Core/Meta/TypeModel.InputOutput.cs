4b204db8e5b2c028a5994d937bfd9cb5297bf54f
mgravell <marc.gravell@gmail.com>
2019-10-18 09:17:47 +0100

    add IBufferWriter<byte>, ReadOnlyMemory<byte> and ReadOnlySequence<byte> to the capability API

--- src/protobuf-net.Core/Meta/TypeModel.InputOutput.cs
+++ src/protobuf-net.Core/Meta/TypeModel.InputOutput.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Buffers;
 using System.IO;
 
 namespace ProtoBuf.Meta
@@ -7,18 +8,34 @@ namespace ProtoBuf.Meta
         IProtoInput<Stream>,
         IProtoInput<ArraySegment<byte>>,
         IProtoInput<byte[]>,
-        IProtoOutput<Stream>
+        IProtoInput<ReadOnlyMemory<byte>>,
+        IProtoInput<ReadOnlySequence<byte>>,
+        IProtoOutput<Stream>,
+        IProtoOutput<IBufferWriter<byte>>
     {
         T IProtoInput<Stream>.Deserialize<T>(Stream source, T value, object userState)
             => Deserialize<T>(source, value, userState);
-
-        void IProtoOutput<Stream>.Serialize<T>(Stream destination, T value, object userState)
-            => Serialize<T>(destination, value, userState);
 
         T IProtoInput<ArraySegment<byte>>.Deserialize<T>(ArraySegment<byte> source, T value, object userState)
             => Deserialize<T>(new ReadOnlyMemory<byte>(source.Array, source.Offset, source.Count), value, userState);
 
         T IProtoInput<byte[]>.Deserialize<T>(byte[] source, T value, object userState)
             => Deserialize<T>(new ReadOnlyMemory<byte>(source), value, userState);
+
+        T IProtoInput<ReadOnlyMemory<byte>>.Deserialize<T>(ReadOnlyMemory<byte> source, T value, object userState)
+            => Deserialize<T>(source, value, userState);
+
+        T IProtoInput<ReadOnlySequence<byte>>.Deserialize<T>(ReadOnlySequence<byte> source, T value, object userState)
+            => Deserialize<T>(source, value, userState);
+
+
+
+
+        void IProtoOutput<Stream>.Serialize<T>(Stream destination, T value, object userState)
+            => Serialize<T>(destination, value, userState);
+
+
+        void IProtoOutput<IBufferWriter<byte>>.Serialize<T>(IBufferWriter<byte> destination, T value, object userState)
+            => Serialize<T>(destination, value, userState);
     }
 }
--- src/protobuf-net.Test/InputOutputAPI.cs
+++ src/protobuf-net.Test/InputOutputAPI.cs
@@ -1,5 +1,7 @@
-﻿using ProtoBuf.Meta;
+﻿using Pipelines.Sockets.Unofficial.Buffers;
+using ProtoBuf.Meta;
 using System;
+using System.Buffers;
 using System.IO;
 using Xunit;
 
@@ -30,33 +32,48 @@ namespace ProtoBuf
         [Fact]
         public void CanSerializeViaInputOutputAPI()
         {
-            using (var ms = new MemoryStream())
-            {
-                IProtoOutput<Stream> output = RuntimeTypeModel.Default;
-                var orig = new SomeModel { Id = 42 };
-                output.Serialize(ms, orig);
+            using var ms = new MemoryStream();
+            IProtoOutput<Stream> output = RuntimeTypeModel.Default;
+            var orig = new SomeModel { Id = 42 };
+            output.Serialize(ms, orig);
 
-                ms.Position = 0;
+            ms.Position = 0;
 
-                IProtoInput<Stream> input = RuntimeTypeModel.Default;
-                var clone = input.Deserialize<SomeModel>(ms);
+            IProtoInput<Stream> input = RuntimeTypeModel.Default;
+            var clone = input.Deserialize<SomeModel>(ms);
 
-                Assert.NotSame(orig, clone);
-                Assert.Equal(42, clone.Id);
+            Assert.NotSame(orig, clone);
+            Assert.Equal(42, clone.Id);
 
-                IProtoInput<byte[]> arrayInput = RuntimeTypeModel.Default;
-                clone = arrayInput.Deserialize<SomeModel>(ms.ToArray());
+            IProtoInput<byte[]> arrayInput = RuntimeTypeModel.Default;
+            clone = arrayInput.Deserialize<SomeModel>(ms.ToArray());
 
-                Assert.NotSame(orig, clone);
-                Assert.Equal(42, clone.Id);
+            Assert.NotSame(orig, clone);
+            Assert.Equal(42, clone.Id);
 
-                var segment = new ArraySegment<byte>(ms.GetBuffer(), 0, (int)ms.Length);
-                IProtoInput<ArraySegment<byte>> segmentInput = RuntimeTypeModel.Default;
-                clone = segmentInput.Deserialize<SomeModel>(segment);
+            var segment = new ArraySegment<byte>(ms.GetBuffer(), 0, (int)ms.Length);
+            IProtoInput<ArraySegment<byte>> segmentInput = RuntimeTypeModel.Default;
+            clone = segmentInput.Deserialize<SomeModel>(segment);
 
-                Assert.NotSame(orig, clone);
-                Assert.Equal(42, clone.Id);
-            }
+            Assert.NotSame(orig, clone);
+            Assert.Equal(42, clone.Id);
+        }
+
+        [Fact]
+        public void CanSerializeViaInputOutputAPI_Buffers()
+        {
+            using var buffer = BufferWriter<byte>.Create();
+            IProtoOutput<IBufferWriter<byte>> output = RuntimeTypeModel.Default;
+
+            var orig = new SomeModel { Id = 42 };
+            output.Serialize(buffer, orig);
+
+            using var payload = buffer.Flush();
+            IProtoInput<ReadOnlySequence<byte>> input = RuntimeTypeModel.Default;
+            var clone = input.Deserialize<SomeModel>(payload.Value);
+
+            Assert.NotSame(orig, clone);
+            Assert.Equal(42, clone.Id);
         }
     }
 }
