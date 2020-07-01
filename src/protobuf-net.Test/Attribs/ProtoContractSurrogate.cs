35727a1a65603f4f64437aff5c456eab03a2497b
Olivier Coanet <olivier.coanet@gmail.com>
2020-07-01 17:22:24 +0200

    support open generic types as surrogates (#446)

--- src/protobuf-net.Test/Attribs/ProtoContractSurrogate.cs
+++ src/protobuf-net.Test/Attribs/ProtoContractSurrogate.cs
@@ -49,5 +49,79 @@ namespace ProtoBuf.unittest.Attribs
 
             Assert.Equal(tableColumn.Key, clone.Key);
         }
+
+        [ProtoContract(Surrogate = typeof(ImmutableGenericType1Surrogate<>))]
+        public class ImmutableGenericType1<T>
+        {
+            public ImmutableGenericType1(T value)
+            {
+                Value = value;
+            }
+
+            public T Value { get; }
+        }
+
+        [ProtoContract]
+        public class ImmutableGenericType1Surrogate<T>
+        {
+            [ProtoMember(1, IsRequired = true)]
+            public T Value { get; set; }
+
+            public static implicit operator ImmutableGenericType1<T>(ImmutableGenericType1Surrogate<T> surrogate)
+            {
+                return surrogate == null ? null : new ImmutableGenericType1<T>(surrogate.Value);
+            }
+
+            public static implicit operator ImmutableGenericType1Surrogate<T>(ImmutableGenericType1<T> source)
+            {
+                return source == null ? null : new ImmutableGenericType1Surrogate<T> { Value = source.Value };
+            }
+        }
+
+        [Fact]
+        public void ImmutableGenericSerialization()
+        {
+            var instance = new ImmutableGenericType1<string>("XYZ!");
+            var clone = RuntimeTypeModel.Default.DeepClone(instance);
+
+            Assert.Equal(instance.Value, clone.Value);
+        }
+
+        [ProtoContract(Surrogate = typeof(ImmutableGenericType2<>.Surrogate))]
+        public class ImmutableGenericType2<T>
+        {
+            public ImmutableGenericType2(T value)
+            {
+                Value = value;
+            }
+
+            public T Value { get; }
+
+            [ProtoContract]
+            public class Surrogate
+            {
+                [ProtoMember(1, IsRequired = true)]
+                public T Value { get; set; }
+
+                public static implicit operator ImmutableGenericType2<T>(Surrogate surrogate)
+                {
+                    return surrogate == null ? null : new ImmutableGenericType2<T>(surrogate.Value);
+                }
+
+                public static implicit operator Surrogate(ImmutableGenericType2<T> source)
+                {
+                    return source == null ? null : new Surrogate { Value = source.Value };
+                }
+            }
+        }
+
+        [Fact]
+        public void ImmutableGenericNestedSerialization()
+        {
+            var instance = new ImmutableGenericType2<string>("XYZ!");
+            var clone = RuntimeTypeModel.Default.DeepClone(instance);
+
+            Assert.Equal(instance.Value, clone.Value);
+        }
     }
 }
