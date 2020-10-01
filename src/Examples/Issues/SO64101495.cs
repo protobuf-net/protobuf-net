69c19176c68ed1ed7d520e1f1a87e81261637696
mgravell <marc.gravell@gmail.com>
2020-10-01 08:08:47 +0100

    add baseline and binary assert to SO64101495 example

--- src/Examples/Issues/SO64101495.cs
+++ src/Examples/Issues/SO64101495.cs
@@ -1,6 +1,8 @@
 ﻿using ProtoBuf.Meta;
 using ProtoBuf.Serializers;
+using ProtoBuf.WellKnownTypes;
 using System;
+using System.IO;
 using System.Net;
 using Xunit;
 using Xunit.Abstractions;
@@ -9,6 +11,15 @@ namespace ProtoBuf.Issues
 {
     public class SO64101495
     {
+        /*
+         Verify 4 different ways of mapping a type to a primitive
+
+        1. via AllowParseableTypes (let the engine handle the parse/format)
+        2. via SetSurrogate (explicitly provide the conversion)
+        3. via SerializerType (write our own serializer)
+        4. via a separate DTO that matches what we want to serialize (baseline)
+        */
+
         public SO64101495(ITestOutputHelper log)
             => Log = log;
         private readonly ITestOutputHelper Log;
@@ -32,7 +43,8 @@ message Example {
 }
 ", schema);
 
-            var obj = new Example { Date = DateTime.Today, Ip = IPAddress.Parse("114.43.32.145"), ExampleDescription = "foo" };
+            var obj = new Example { Date = When, Ip = IPAddress.Parse(TestIPAddress), ExampleDescription = "foo" };
+            Assert.Equal(ExpectedHex, GetHex(model, obj));
             var clone = model.DeepClone(obj);
             Assert.NotSame(obj, clone);
             Assert.Equal(obj.Date, clone.Date);
@@ -59,7 +71,8 @@ message Example {
 }
 ", schema);
 
-            var obj = new Example { Date = DateTime.Today, Ip = IPAddress.Parse("114.43.32.145"), ExampleDescription = "foo" };
+            var obj = new Example { Date = When, Ip = IPAddress.Parse(TestIPAddress), ExampleDescription = "foo" };
+            Assert.Equal(ExpectedHex, GetHex(model, obj));
             var clone = model.DeepClone(obj);
             Assert.NotSame(obj, clone);
             Assert.Equal(obj.Date, clone.Date);
@@ -91,13 +104,54 @@ message Example {
 }
 ", schema);
 
-            var obj = new Example { Date = DateTime.Today, Ip = IPAddress.Parse("114.43.32.145"), ExampleDescription = "foo" };
+            var obj = new Example { Date = When, Ip = IPAddress.Parse(TestIPAddress), ExampleDescription = "foo" };
+            Assert.Equal(ExpectedHex, GetHex(model, obj));
             var clone = model.DeepClone(obj);
             Assert.NotSame(obj, clone);
             Assert.Equal(obj.Date, clone.Date);
             Assert.Equal(obj.Ip, clone.Ip);
             Assert.Equal(obj.ExampleDescription, clone.ExampleDescription);
         }
+
+        [Fact]
+        public void RoundTripIPAddessEffectiveCompat()
+        {
+            var model = RuntimeTypeModel.Create();
+            var schema = model.GetSchema(typeof(EffectiveExample));
+            Log?.WriteLine(schema);
+            Assert.Equal(@"syntax = ""proto3"";
+package ProtoBuf.Issues;
+import ""google/protobuf/timestamp.proto"";
+
+message Example {
+   .google.protobuf.Timestamp Date = 1;
+   string Ip = 2;
+   string ExampleDescription = 3;
+}
+", schema);
+
+            var obj = new EffectiveExample { Date = When, Ip = TestIPAddress, ExampleDescription = "foo" };
+            var hex = GetHex(model, obj);
+            Log?.WriteLine(hex);
+            Assert.Equal(ExpectedHex, hex);
+            var clone = model.DeepClone(obj);
+            Assert.NotSame(obj, clone);
+            Assert.Equal(obj.Date, clone.Date);
+            Assert.Equal(obj.Ip, clone.Ip);
+            Assert.Equal(obj.ExampleDescription, clone.ExampleDescription);
+        }
+
+        static string GetHex<T>(TypeModel model, T value)
+        {
+            using var ms = new MemoryStream();
+            model.Serialize(ms, value);
+            return BitConverter.ToString(ms.GetBuffer(), 0, (int)ms.Length);
+        }
+
+        private static readonly DateTime When = new DateTime(2020, 10, 1, 8, 0, 0, DateTimeKind.Utc);
+        private const string
+            TestIPAddress = "114.43.32.145",
+            ExpectedHex = @"0A-06-08-80-99-D6-FB-05-12-0D-31-31-34-2E-34-33-2E-33-32-2E-31-34-35-1A-03-66-6F-6F";
 
         public sealed class IPAddressSerializer : ISerializer<IPAddress>
         {
@@ -118,6 +172,17 @@ message Example {
             public DateTime Date { get; set; }
             [ProtoMember(2)]
             public IPAddress Ip { get; set; }
+            [ProtoMember(3)]
+            public string ExampleDescription { get; set; }
+        }
+
+        [ProtoContract(Name = "Example")]
+        public class EffectiveExample
+        {
+            [ProtoMember(1)]
+            public Timestamp Date { get; set; }
+            [ProtoMember(2)]
+            public string Ip { get; set; }
             [ProtoMember(3)]
             public string ExampleDescription { get; set; }
         }
