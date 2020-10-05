86761dede9c893e465171f6708e0f387c617240f
mgravell <marc.gravell@gmail.com>
2020-10-05 08:19:20 +0100

    add stub for record types; can't test currently

---
+++ src/protobuf-net.Test/RecordTypeTests.cs
@@ -0,0 +1,17 @@
+﻿namespace ProtoBuf.Test
+{
+    public class RecordTypeTests
+    {
+        // no compiler currently works with these
+
+        /*
+        public data class LongHand
+        {
+            public string FirstName { get; init; }
+            public string LastName { get; init; }
+        }
+        public data class FieldsSpecified { string FirstName; string LastName; }
+        public data class Positional(string FirstName, string LastName);
+        */
+    }
+}
