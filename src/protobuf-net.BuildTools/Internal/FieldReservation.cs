45144b06497439415d4660b63fe0edabab3e2214
mgravell <marc.gravell@gmail.com>
2020-11-29 13:13:01 +0000

    add missing files!

---
+++ src/protobuf-net.BuildTools/Internal/FieldReservation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ProtoBuf.BuildTools.Internal
+{
+    internal readonly struct FieldReservation : IEquatable<FieldReservation>
+    {
+        private readonly string? _name;
+        private readonly int _from, _to;
+        public FieldReservation(int from) : this(from, from) { }
+
+        public FieldReservation(int from, int to)
+        {
+            _from = from;
+            _to = to;
+            _name = null;
+        }
+
+        public FieldReservation(string name)
+        {
+            _from = _to = default;
+            _name = name;
+        }
+
+        public override string ToString()
+        {
+            if (_name is not null) return $"'{_name}'";
+            return _from == _to ? _from.ToString() : $"[{_from}-{_to}]";
+        }
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 23 + _name?.GetHashCode() ?? 0;
+                hash = hash * 23 + _from;
+                hash = hash * 23 + _to;
+                return hash;
+            }
+        }
+        public bool Includes(string name)
+            => _name is not null && _name == name;
+        public bool Includes(int number)
+            => _name is null && _from <= number && _to >= number;
+
+        bool IEquatable<FieldReservation>.Equals(FieldReservation other)
+            => Equals(in other);
+        public bool Equals(in FieldReservation other)
+            => _name == other._name && _from == other._from && _to == other._to;
+        public override bool Equals(object obj)
+            => obj is FieldReservation other && Equals(in other);
+
+        internal bool Overlaps(in FieldReservation other)
+        {
+            if (_name is not null || other._name is not null) return _name == other._name;
+
+            // do we include either end of the other? (or both)
+            if (Includes(other._from) || Includes(other._to)) return true;
+
+            // does the other totally include us?
+            return other._from < _from && other._to > _to;
+        }
+    }
+}
---
+++ src/protobuf-net.BuildTools/Internal/TypeContext.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Generic;
+
+namespace ProtoBuf.BuildTools.Internal
+{
+    internal sealed class TypeContext
+    {
+        private HashSet<int>? _knownFieldNumbers;
+        private HashSet<string>? _knownFieldNames;
+        private List<FieldReservation>? _reservations;
+
+        public void AddReservation(ref SyntaxNodeAnalysisContext context, in FieldReservation reservation, ISymbol? symbol)
+        {
+            if (OverlapsReservation(in reservation, out var existing))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(
+                    descriptor: ProtobufFieldAnalyzer.DuplicateReservation,
+                    location: Utils.PickLocation(ref context, symbol),
+                    messageArgs: new object[] { reservation, existing },
+                    additionalLocations: null,
+                    properties: null
+                ));
+            }
+            (_reservations ??= new List<FieldReservation>()).Add(reservation);
+        }
+        public void AddKnownField(string? name, int number)
+        {
+            if (name is not null)
+            {
+                (_knownFieldNames ??= new HashSet<string>()).Add(name);
+            }
+            (_knownFieldNumbers ??= new HashSet<int>()).Add(number);
+        }
+
+        public bool OverlapsField(string name)
+            => _knownFieldNames is not null && _knownFieldNames.Contains(name);
+
+        public bool OverlapsField(int number)
+            => _knownFieldNumbers is not null && _knownFieldNumbers.Contains(number);
+
+        public bool OverlapsReservation(in FieldReservation reservation, out FieldReservation overlap)
+        {
+            if (_reservations is not null)
+            {
+                foreach (var existing in _reservations)
+                {
+                    if (existing.Overlaps(reservation))
+                    {
+                        overlap = reservation;
+                        return true;
+                    }
+                }
+            }
+            overlap = default;
+            return false;
+        }
+
+        public bool OverlapsReservation(string name, out FieldReservation overlap)
+        {
+            if (_reservations is not null)
+            {
+                foreach (var existing in _reservations)
+                {
+                    if (existing.Includes(name))
+                    {
+                        overlap = existing;
+                        return true;
+                    }
+                }
+            }
+            overlap = default;
+            return false;
+        }
+
+        public bool OverlapsReservation(int number, out FieldReservation overlap)
+        {
+            if (_reservations is not null)
+            {
+                foreach (var existing in _reservations)
+                {
+                    if (existing.Includes(number))
+                    {
+                        overlap = existing;
+                        return true;
+                    }
+                }
+            }
+            overlap = default;
+            return false;
+        }
+    }
+}
