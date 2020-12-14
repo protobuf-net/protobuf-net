4947fb7745f08598e7d658f5a2a90f3582116783
Adam May <adam.may@lifetrading.com.au>
2020-12-15 10:10:35 +1100

    Updating unit test

--- src/Examples/Issues/Issue740.cs
+++ src/Examples/Issues/Issue740.cs
@@ -18,16 +18,20 @@ package MainPackage;
 
 import ""child_proto_text.proto"";
 
+message ParentRequest { }
+message ParentReply { }
+
 service MyService {
-    rpc MyMethod(ChildPackage.Request) returns (ChildPackage.Reply);
+    rpc MyMethodExternalMessage(ChildPackage.ChildRequest) returns (ChildPackage.ChildReply);
+    rpc MyMethodInternalMessage(ParentRequest) returns (ParentReply);
 }
 ";
 
             const string childProtoText = @"syntax = ""proto3"";
 package ChildPackage;
 
-message Request { }
-message Reply { }
+message ChildRequest { }
+message ChildReply { }
 ";
 
             var set = new FileDescriptorSet();
@@ -48,11 +52,17 @@ message Reply { }
             // Find the request/reply types from the service interface
             var mainProtoSource = sourceFiles[0];
 
-            var regex = new Regex(@"$\s+global::System\.Threading\.Tasks\.ValueTask<(?<Reply>\S+)> MyMethodAsync\((?<Request>\S+) value,", RegexOptions.Multiline);
+            var regex = new Regex(@"$\s+global::System\.Threading\.Tasks\.ValueTask<(?<Reply>\S+)> MyMethodExternalMessageAsync\((?<Request>\S+) value,", RegexOptions.Multiline);
             var match = regex.Match(mainProtoSource);
 
-            Assert.Equal("global::ChildPackage.Reply", match.Groups["Reply"].Value);
-            Assert.Equal("global::ChildPackage.Request", match.Groups["Request"].Value);
+            Assert.Equal("global::ChildPackage.ChildReply", match.Groups["Reply"].Value);
+            Assert.Equal("global::ChildPackage.ChildRequest", match.Groups["Request"].Value);
+
+            regex = new Regex(@"$\s+global::System\.Threading\.Tasks\.ValueTask<(?<Reply>\S+)> MyMethodInternalMessageAsync\((?<Request>\S+) value,", RegexOptions.Multiline);
+            match = regex.Match(mainProtoSource);
+
+            Assert.Equal("ParentReply", match.Groups["Reply"].Value);
+            Assert.Equal("ParentRequest", match.Groups["Request"].Value);
         }
     }
 }
