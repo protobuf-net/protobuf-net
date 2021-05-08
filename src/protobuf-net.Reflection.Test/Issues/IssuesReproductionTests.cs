9be24d7bf1c6c2eb34ca95266e8a845a8d04ea00
imazurenko <imazurenko@spotware.com>
2021-05-08 09:12:54 +0300

    add test to reproduce #790 issue

---
+++ src/protobuf-net.Reflection.Test/Issues/IssuesReproductionTests.cs
@@ -0,0 +1,25 @@
+﻿using Google.Protobuf.Reflection;
+using System.IO;
+using Xunit;
+
+namespace ProtoBuf
+{
+    public class IssuesReproductionTests
+    {
+        [Fact]
+        public void CorrectlyParsesMessagesWithQuotationMarkInComments()
+        {
+            var set = new FileDescriptorSet();
+            using var fileWithoutQuotationMarkInComments =
+                File.OpenText(Path.Combine("Issues", "messagesWithoutQuotationMarkInComments.proto"));
+            using var fileWithQuotationMarkInComments =
+                File.OpenText(Path.Combine("Issues", "messagesWithQuotationMarkInComments.proto"));
+            set.Add("messagesWithoutQuotationMarkInComments.proto", true, fileWithoutQuotationMarkInComments);
+            set.Add("messagesWithQuotationMarkInComments.proto", true, fileWithQuotationMarkInComments);
+
+            set.Process();
+
+            Assert.Equal(set.Files[0].MessageTypes.Count, set.Files[1].MessageTypes.Count);
+        }
+    }
+}
