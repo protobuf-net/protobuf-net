8ea44ed61c0f1fedc7e9cc73c5c707cfe616ed83
imazurenko <imazurenko@spotware.com>
2021-05-09 07:00:51 +0300

    rename test class 2, this changes was not committed for some reasons

---
+++ src/protobuf-net.Reflection.Test/Issues/Issue790.cs
@@ -0,0 +1,39 @@
+﻿using Google.Protobuf.Reflection;
+using System.IO;
+using Xunit;
+
+namespace ProtoBuf
+{
+    public class IssuesReproductionTests
+    {
+        [Fact]
+        public void CorrectlyParsesMessagesWithQuotationMarkInComments()
+        {
+            var set = new FileDescriptorSet();
+            using var fileWithoutQuotationMarkInComments =
+                File.OpenText(Path.Combine("Issues", "messagesWithoutQuotationMarkInComments.proto"));
+            using var fileWithQuotationMarkInComments =
+                File.OpenText(Path.Combine("Issues", "messagesWithQuotationMarkInComments.proto"));
+            set.Add("messagesWithoutQuotationMarkInComments.proto", true, fileWithoutQuotationMarkInComments);
+            set.Add("messagesWithQuotationMarkInComments.proto", true, fileWithQuotationMarkInComments);
+
+            set.Process();
+
+            Assert.Equal(set.Files[0].MessageTypes.Count, set.Files[1].MessageTypes.Count);
+        }
+
+        [Fact]
+        public void CorrectlyParsesMessageWithQuotationMarkInComment()
+        {
+            var set = new FileDescriptorSet();
+
+            using var fileWithQuotationMarkInComments =
+                File.OpenText(Path.Combine("Issues", "messagesWithQuotationMarkInComments2.proto"));
+            set.Add("messagesWithQuotationMarkInComments.proto", true, fileWithQuotationMarkInComments);
+
+            set.Process();
+
+            Assert.Single(set.Files[0].MessageTypes);
+        }
+    }
+}
