df2ff898401c8749e698c8befd2fd6bb13b2588c
Marc Gravell <marc.gravell@gmail.com>
2022-05-10 12:25:49 +0100

    allow IEnumerable<T> properties to be cleared by using the same behaviour as null would have used (#913)

---
+++ src/protobuf-net.Test/Issues/OverwriteEnumerable.cs
@@ -0,0 +1,35 @@
+﻿using ProtoBuf.Meta;
+using ProtoBuf.unittest;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace ProtoBuf.Test.Issues
+{
+    public class OverwriteEnumerable
+    {
+        [Fact]
+        public void Execute()
+        {
+            var model = RuntimeTypeModel.Create();
+            model.AutoCompile = false;
+            ExecuteImpl(model);
+            model.CompileInPlace();
+            ExecuteImpl(model);
+            ExecuteImpl(model.CompileAndVerify());
+        }
+        private void ExecuteImpl(TypeModel model)
+        {
+            var obj = new TypeWithEnumerableProperty { Values = new List<string> { "val1", "val2", "val3" } };
+            var clone = model.DeepClone(obj);
+            Assert.NotSame(obj, clone);
+            Assert.Equal("val1,val2,val3", string.Join(",", clone.Values));
+        }
+    }
+    [ProtoContract]
+    public partial class TypeWithEnumerableProperty
+    {
+        [ProtoMember(1, OverwriteList = true)]
+        public IEnumerable<string> Values { get; set; } = Enumerable.Empty<string>();
+    }
+}
