c5171775b1979be6ac40f6be2dd7813d20157aaf
Jason Nelson <jason@carbonmade.com>
2022-08-18 10:23:26 -0700

    Add nullable polyfills

---
+++ src/protobuf-net.Core/Polyfills/NullableAttributes.cs
@@ -0,0 +1,23 @@
+﻿#if NETSTANDARD2_0 || NETFRAMEWORK
+
+namespace System.Diagnostics.CodeAnalysis;
+
+internal sealed class NotNullAttribute : Attribute { }
+
+internal sealed class DoesNotReturnAttribute : Attribute { }
+
+internal sealed class NotNullIfNotNullAttribute : Attribute
+{
+    public NotNullIfNotNullAttribute(string parameterName) => ParameterName = parameterName;
+
+    public string ParameterName { get; }
+}
+
+internal sealed class NotNullWhenAttribute : Attribute
+{
+    public NotNullWhenAttribute(bool returnValue) => ReturnValue = returnValue;
+
+    public bool ReturnValue { get; }
+}
+
+#endif
