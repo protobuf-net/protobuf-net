6e1df7210a2336171370c19994ee43347a678fd5
Dmitrii Korolev <dmkorolev@microsoft.com>
2022-09-19 18:09:40 +0200

    add custom type for codegen

---
+++ src/protobuf-net.Reflection/Internal/CodeGen/CodeGenCustomType.cs
@@ -0,0 +1,17 @@
+﻿using ProtoBuf.Reflection.Internal.CodeGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtoBuf.Internal.CodeGen
+{
+    internal class CodeGenCustomType : CodeGenType
+    {
+        public CodeGenCustomType(string name, string fullyQualifiedPrefix)
+            : base(name, fullyQualifiedPrefix)
+        {
+        }
+    }
+}
