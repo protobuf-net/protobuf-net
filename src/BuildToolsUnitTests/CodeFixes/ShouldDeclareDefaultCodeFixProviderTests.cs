f96be49819ed1350bae7f60a56135765bf778895
Dmitrii Korolev <dmkorolev@microsoft.com>
2023-02-14 20:14:49 +0100

    setup tests for code fixx

---
+++ src/BuildToolsUnitTests/CodeFixes/ShouldDeclareDefaultCodeFixProviderTests.cs
@@ -0,0 +1,48 @@
+﻿using ProtoBuf.BuildTools.Analyzers;
+using System.Threading.Tasks;
+using Xunit;
+
+using VerifyCS = BuildToolsUnitTests.CodeFixes.Infra.CSharpCodeFixVerifier<
+    ProtoBuf.BuildTools.Analyzers.DataContractAnalyzer,
+    ProtoBuf.CodeFixes.ShouldDeclareDefaultCodeFixProvider>;
+
+namespace BuildToolsUnitTests.CodeFixes
+{
+    public class ShouldDeclareDefaultCodeFixProviderTests
+    {
+        [Fact]
+        public async Task Assert()
+        {
+            var testCode = @"
+                namespace TestNamespace
+                {
+                    [ProtoContract]
+                    public class Model
+                    {
+                        [ProtoMember(1)]
+                        public int Items { get; set; } = 1;
+                    }
+                }
+            ";
+
+            var appliedCodeFix = @"
+                namespace TestNamespace
+                {
+                    [ProtoContract]
+                    public class Model
+                    {
+                        [ProtoMember(1)]
+                        [DefaultValue(1)]
+                        public int Items { get; set; } = 1;
+                    }
+                }
+            ";
+
+            var expected = VerifyCS.Diagnostic(DataContractAnalyzer.ShouldDeclareDefault.Id)
+                .WithSpan(0, 1, 1, 1)
+                .WithMessage(string.Format(DataContractAnalyzer.ShouldDeclareDefault.MessageFormat, "qwe", "qwe"));
+
+            await VerifyCS.VerifyCodeFixAsync(testCode, expected, appliedCodeFix);
+        }
+    }
+}
---
+++ src/protobuf-net.BuildTools/CodeFixes/ShouldDeclareDefaultCodeFixProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using ProtoBuf.BuildTools.Analyzers;
+using System;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Threading.Tasks;
+
+namespace ProtoBuf.CodeFixes
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(ShouldDeclareDefaultCodeFixProvider)), Shared]
+    public class ShouldDeclareDefaultCodeFixProvider : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(DataContractAnalyzer.ShouldDeclareDefault.Id);
+
+        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var a = context;
+
+            throw new NotImplementedException();
+        }
+    }
+}
