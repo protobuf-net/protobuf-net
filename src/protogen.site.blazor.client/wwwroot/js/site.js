dba633743438b568284b6e1102bc386a3b3d932c
Marc Gravell <marc.gravell@gmail.com>
2023-02-21 14:32:14 +0000

    allow generation with warnings; update monaco

--- src/protogen.site.blazor.client/wwwroot/js/site.js
+++ src/protogen.site.blazor.client/wwwroot/js/site.js
@@ -1,5 +1,5 @@
 ﻿
-require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.18.0/min/vs' } });
+require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.35.0/min/vs' } });
 
 // Before loading vs/editor/editor.main, define a global MonacoEnvironment that overwrites
 // the default worker url location (used when creating WebWorkers). The problem here is that
@@ -9,9 +9,9 @@ window.MonacoEnvironment = {
     getWorkerUrl: function (workerId, label) {
         return `data:text/javascript;charset=utf-8,${encodeURIComponent(`
                                             self.MonacoEnvironment = {
-                                              baseUrl: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.18.0/min/'
+                                              baseUrl: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.35.0/min/'
                                             };
-                                            importScripts('https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.18.0/min/vs/base/worker/workerMain.js');`
+                                            importScripts('https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.35.0/min/vs/base/worker/workerMain.js');`
         )}`;
     }
 };
