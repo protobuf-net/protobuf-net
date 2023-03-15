40eedcdbeb7331704a2c99a7591aef0e81e89222
Marc Gravell <marc.gravell@gmail.com>
2023-03-15 20:13:03 +0000

    use manually implented equivalent to nail expected bytes

--- src/protobuf-net.Test/NullCollections.cs
+++ src/protobuf-net.Test/NullCollections.cs
@@ -19,9 +19,9 @@ namespace ProtoBuf.Test
 
         [Theory]
         [InlineData(-1, "")]
-        [InlineData(0, "22-00")] // guess, something like
-        [InlineData(1, "22-04-0A-02-08-00")] // guess, something like
-        [InlineData(10)]
+        [InlineData(0, "22-00")]
+        [InlineData(1, "22-04-0A-02-08-00")]
+        [InlineData(10, "22-28-0A-02-08-00-0A-02-08-01-0A-02-08-02-0A-02-08-03-0A-02-08-04-0A-02-08-05-0A-02-08-06-0A-02-08-07-0A-02-08-08-0A-02-08-09")]
         public void TestWithNullWrappedCollection(int count, string? hex = null) => Test<WithNullWrappedCollection>(count, true, hex);
 
         [Theory]
@@ -31,18 +31,25 @@ namespace ProtoBuf.Test
         [InlineData(10, "22-02-08-00-22-02-08-01-22-02-08-02-22-02-08-03-22-02-08-04-22-02-08-05-22-02-08-06-22-02-08-07-22-02-08-08-22-02-08-09")]
         public void TestVanilla(int count, string? hex = null) => Test<Vanilla>(count, false, hex);
 
-        private void Test<T>(int count, bool withAttrib, string? expectedHex) where T : class, ITestScenario, new()
+        [Theory]
+        [InlineData(-1, "")]
+        [InlineData(0, "22-00")]
+        [InlineData(1, "22-04-0A-02-08-00")]
+        [InlineData(10, "22-28-0A-02-08-00-0A-02-08-01-0A-02-08-02-0A-02-08-03-0A-02-08-04-0A-02-08-05-0A-02-08-06-0A-02-08-07-0A-02-08-08-0A-02-08-09")]
+        public void TestManualWrappedEquivalent(int count, string? hex = null) => Test<ManualWrappedEquivalent>(count, true, hex);
+
+        private void Test<T>(int count, bool preserveEmpty, string? expectedHex) where T : class, ITestScenario, new()
         {
             var model = RuntimeTypeModel.Create();
             model.AutoCompile = false;
             model.Add<T>();
-            Test<T>(model, count, withAttrib, expectedHex);
+            Test<T>(model, count, preserveEmpty, expectedHex);
             model.CompileInPlace();
-            Test<T>(model, count, withAttrib, expectedHex);
-            Test<T>(PEVerify.CompileAndVerify(model), count, withAttrib, expectedHex);
+            Test<T>(model, count, preserveEmpty, expectedHex);
+            Test<T>(PEVerify.CompileAndVerify(model), count, preserveEmpty, expectedHex);
         }
 
-        private void Test<T>(TypeModel model, int count, bool withAttrib, string? expectedHex) where T : class, ITestScenario, new()
+        private void Test<T>(TypeModel model, int count, bool preserveEmpty, string? expectedHex) where T : class, ITestScenario, new()
         {
             T obj = new();
             if (count >= 0)
@@ -64,7 +71,7 @@ namespace ProtoBuf.Test
             ms.Position = 0;
             var clone = model.Deserialize<T>(ms);
 
-            if (count < 0 || (count == 0 && !withAttrib))
+            if (count < 0 || (count == 0 && !preserveEmpty))
             {
                 Assert.Null(clone.Foos);
             }
@@ -88,6 +95,42 @@ namespace ProtoBuf.Test
         {
             [ProtoMember(4)]
             public List<Foo>? Foos { get; set; }
+        }
+
+        [ProtoContract]
+        public class ManualWrappedEquivalent : ITestScenario
+        {
+            [ProtoContract]
+            public class WrapperLayer
+            {
+                public WrapperLayer() => Foos = new();
+                public WrapperLayer(List<Foo> foos) => Foos = foos;
+
+                [ProtoMember(1)]
+                public List<Foo> Foos { get; set; }
+            }
+
+            [ProtoMember(4)]
+            public WrapperLayer? Wrapper {get;set;}
+            List<Foo>? ITestScenario.Foos
+            {
+                get => Wrapper?.Foos;
+                set
+                {
+                    if (value is null)
+                    {
+                        Wrapper = null;
+                    }
+                    else if (Wrapper is null)
+                    {
+                        Wrapper = new(value);
+                    }
+                    else
+                    {
+                        Wrapper.Foos = value;
+                    }
+                }
+            }
         }
 
         [ProtoContract]
